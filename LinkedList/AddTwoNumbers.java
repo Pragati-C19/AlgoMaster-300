7a48d048df52d2b7681ff2f645c028ade8dbf998
Pragati Chothe <pragatichothe@gmail.com>
2025-03-15 15:44:23 +0530

    Step 2 : Intution, Pattern or Pseudo code

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -9,11 +9,10 @@ public class AddTwoNumbers {
         }
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         return l1;
     }
-
 
     // Helper function to print the list (for debugging)
     public void printList(ListNode head) {
@@ -34,7 +33,7 @@ public class AddTwoNumbers {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
-        
+
         System.out.println("First number:");
         solution.printList(l1);
 
@@ -48,3 +47,45 @@ public class AddTwoNumbers {
     }
 }
 
+/*
+ * 
+ * Intutions :
+ * 
+ * l1 = [2,4,3] represents 342
+ * l2 = [5,6,4] represents 465
+ * 
+ * 342 + 465 = 807 ➜ Output: [7,0,8]
+ * 
+ * 1. we need to trace from Head to tail bcoz number are reverse
+ * 2. Maintain a carry
+ * 3. when one list is short then treat missing digit as zero
+ * 4. If the final carry is non-zero, create a new node.
+ * 
+ * Pattern behind this :
+ * 
+ * 1. Initialize pointers for list and carry which should be set to zero
+ * 2. Traverse the both list
+ * l1.val + l2.val + carry.val
+ * new digit = sum % 10, carry = sum / 10
+ * store that sum in a node
+ * 
+ * Psuedo Code :
+ * 
+ * 1. Initialize dummy node and carry = 0
+ * 2. While l1 or l2 exists:
+ *      - Get values from l1 and l2 (0 if null)
+ *      - Sum = l1.val + l2.val + carry.val
+ *      - Create new node with (sum % 10)
+ *      - Update carry = sum / 10
+ *      - Move to next nodes in l1, l2, and result
+ * 3. If carry > 0, add final node with carry
+ * 4. Return dummy.next (head of result list)
+ *
+ *
+ * why that sum % 10 and sum / 10
+ *      -  If sum = 15, that time we need only 5 → 15 % 10 = 5
+ *      -  If sum = 15, that time we need to carry 1 → 15 / 10 = 1
+ * 
+ * 
+ * 
+ */
