eff13e5604a779292ca64595bb746ad3ad227b5b
Pragati Chothe <pragatichothe@gmail.com>
2025-03-15 19:33:39 +0530

    🌱 Step 2 : Basic setup added

--- Queues/TimeRequiredToBuy.java
+++ Queues/TimeRequiredToBuy.java
@@ -1,5 +1,31 @@
-public class TimeRequiredToBuy {
+import java.util.*;
 
+public class TimeRequiredToBuy {
+    // Declared a queue
+    private Queue<Integer> queue;
+
+    // Constructor initializes the queue as a LinkedList
+    public TimeRequiredToBuy() {
+        queue = new LinkedList<>();
+    }
+
+    public int timeRequiredToBuy(int[] tickets, int k){
+
+        return k;
+    }
+
+    public static void main(String[] args){
+        TimeRequiredToBuy solution = new TimeRequiredToBuy();
+        
+        int[] tickets1 = {2,3,5};
+        int k1 = 2;
+        System.out.println("Time for k1: " + solution.timeRequiredToBuy(tickets1, k1));
+
+        int[] tickets2 = {5, 1, 1, 1};
+        int k2 = 0;
+        System.out.println("Time for k2: " + solution.timeRequiredToBuy(tickets2, k2));
+
+    }
 }
 
 /*
@@ -26,12 +52,13 @@ public class TimeRequiredToBuy {
  * Input: tickets = [2,3,2], k = 2
  * Output: 6
  * 
- * 1. Initialize time = 0
- * 2. Loop through the queue repeatedly
+ * 1. Initialize the queue 
+ * 2. Track time = 0
+ * 3. Loop through the queue repeatedly
  * - For each person i:
  * - If tickets[i] > 0, they buy one ticket:
- * - tickets[i]--
- * - time++
+ * - Decrement tickets -> tickets[i]--
+ * - Increment time -> time++
  * - If person `k` has tickets[k] == 0 after buying, return the total time
  * 3. Repeat until person `k` finishes
  * 4. Return time
