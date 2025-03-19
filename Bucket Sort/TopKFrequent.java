9ff83b6914147f0c1ce6e2ffa785df9ae634901c
Pragati Chothe <pragatichothe@gmail.com>
2025-03-19 16:13:37 +0530

    ⚡️Step 3: Code Writing | 1hr Late Commit

--- Bucket Sort/TopKFrequent.java
+++ Bucket Sort/TopKFrequent.java
@@ -3,8 +3,48 @@ import java.util.*;
 public class TopKFrequent {
 
     public List<String> topKFrequent(String[] words, int k) {
+        
+        // Count frequency of each word
+        Map<String, Integer> frequencyMap = new HashMap<>();
 
-        return words;
+        for (int i=0; i<words.length; i++){
+            String word = words[i];
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+        }
+        System.out.println("Frequency Map: " + frequencyMap); 
+
+        // Sort words alphabetically
+        List<String> lexicographicalList = new ArrayList<>(frequencyMap.keySet());
+        Collections.sort(lexicographicalList);
+        System.out.println("Lexicographical List: " + lexicographicalList);
+
+        // Bucket sort setup 
+        List<String>[] buckets = new List[words.length + 1];
+
+        for(String word : lexicographicalList) {
+            int freq = frequencyMap.get(word);
+
+            System.out.println("Processing Word and frequency: " + word + " -> " + freq + " ");
+
+            if(buckets[freq] == null){
+                buckets[freq] = new ArrayList<>();
+            }
+
+            buckets[freq].add(word);
+        }
+        System.out.println("Bucket Created: " + Arrays.toString(buckets));
+
+        // Get top k words from highest frequency 
+        List<String> result = new ArrayList<>();
+        for(int i = buckets.length - 1; i >= 0; i--) {
+            if(buckets[i] != null){
+                result.addAll(buckets[i]);
+            }
+        }
+        System.out.println("Result List: " + result.toString());
+
+        // Returning only first k elements
+        return result.subList(0, k);
         
     }
 
