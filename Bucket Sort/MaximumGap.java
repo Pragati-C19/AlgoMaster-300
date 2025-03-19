457ef59428d7d0850619bed0f2a895ecbe1a34d5
Pragati Chothe <pragatichothe@gmail.com>
2025-03-19 17:01:39 +0530

    ⚡️Step 3: Code Writing | 1hr Late Commit

--- Bucket Sort/MaximumGap.java
+++ Bucket Sort/MaximumGap.java
@@ -1,15 +1,72 @@
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MaximumGap {
-    public static int maximumGap(int[] nums) {
+    
+    public int maximumGap(int[] nums) {
 
-        return nums;
+        int n = nums.length;
+
+        // If nums has less than 2 elements, return 0.
+        if (n < 2)
+            return 0;
+
+        // Find min and max values
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            // min Values
+            if (num < minVal) minVal = num;
+
+            // Max Values
+            if (num > maxVal) maxVal = num;
+        }
+
+        // Calculate Bucket size and count
+        int bucketSize = (maxVal - minVal) / (n - 1);
+        int bucketCount = (maxVal - minVal) / bucketSize ;
+
+        // Create buckets to store min and max
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+
+        for (int i = 0; i < bucketCount; i++) {
+            bucketMin[i] = Integer.MAX_VALUE;
+            bucketMax[i] = Integer.MIN_VALUE;
+        }        
+
+        // Place numbers into buckets
+        for(int num : nums){
+            int bucketIndex = (num - minVal) / bucketSize;
+
+            bucketMin[bucketIndex] = Math.min(bucketMin[bucketIndex], num);
+            bucketMax[bucketIndex] = Math.max(bucketMax[bucketIndex], num);
+        }
+
+        // Calculate maximum gap
+        int maxGap = 0;
+        int prevBucketMax = minVal;
+        for (int i = 0; i < bucketCount; i++) {
+
+            // Skip empthy buckets
+            if (bucketMin[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            // Gap is from prev bucket Max to current bucket's min
+            maxGap = Math.max(maxGap, bucketMin[i] - prevBucketMax);
+            prevBucketMax = bucketMax[i];
+        }
+
+        return maxGap;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MaximumGap solution = new MaximumGap();
 
-        int[] nums1 = {3, 6, 9, 1};
-        int[] nums2 = {10};
+        int[] nums1 = { 3, 6, 9, 1 };
+        int[] nums2 = { 10 };
 
         System.out.println("Result1: " + solution.maximumGap(nums1) + "\n");
         System.out.println("Result2: " + solution.maximumGap(nums2) + "\n");
@@ -32,6 +89,7 @@ public class MaximumGap {
  * 
  * 1. Find the minimum and maximum elements in the array.
  * 2️. Calculate bucket size and number of buckets:
+ * - n = length of nums
  * - Bucket size = [max - min] / (n - 1)
  * - Number of buckets = [max - min] / (bucket size)
  * 3. Place each number into its corresponding bucket
