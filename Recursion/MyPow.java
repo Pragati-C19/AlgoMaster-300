cf5a4b80c690a124c90e1c978c6206951982f526
Pragati Chothe <pragatichothe@gmail.com>
2025-03-19 19:36:23 +0530

    ⚡️Step 3: Code Writing | 1hr Late Commit

--- Recursion/MyPow.java
+++ Recursion/MyPow.java
@@ -3,6 +3,29 @@ public class MyPow {
 
     public double myPow(double x, int n) {
         
+        if(n == 0) return 1;
+
+        // Handle negative powers
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        double result = 1;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                result = result * x;
+            }
+
+            // square the base
+            x = x * x;
+            
+            // Halve the power
+            n = n / 2;
+        }
+
+        return result;
     }
 
     public static void main(String[] args){
