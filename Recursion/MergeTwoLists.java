35b4da79e3de203e7f4976f483bfe73659f4fb32
Pragati Chothe <pragatichothe@gmail.com>
2025-03-19 19:15:31 +0530

    ⚡️Step 3: Code Writing | 1hr Late Commit

--- Recursion/MergeTwoLists.java
+++ Recursion/MergeTwoLists.java
@@ -11,7 +11,32 @@ public class MergeTwoLists {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
+        ListNode dummy = new ListNode(0);    // Dummy Node for result
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            
+            if (list1.val < list2.val ) {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        }
+
+        if (list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next;
     }
 
     // Helper function to print the list (for debugging)
