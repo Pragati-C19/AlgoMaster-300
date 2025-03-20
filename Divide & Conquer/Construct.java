4486dfc938d198529a63c91f5fc485ca16461aa8
Pragati Chothe <pragatichothe@gmail.com>
2025-03-20 16:51:42 +0530

    🌱 Step 2 : Basic setup added

--- Divide & Conquer/Construct.java
+++ Divide & Conquer/Construct.java
@@ -1,6 +1,57 @@
 import java.util.*;;
 
 public class Construct {
+    private class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+    
+        public Node() {}
+    
+        public Node(boolean _val, boolean _isLeaf) {
+            val = _val;
+            isLeaf = _isLeaf;
+        }
+    
+        public Node(boolean _val, boolean _isLeaf, Node _topLeft, Node _topRight, Node _bottomLeft, Node _bottomRight) {
+            val = _val;
+            isLeaf = _isLeaf;
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+        }
+    } 
+
+    public Node construct(int[][] grid) {
+        
+        return 1;
+    }
+
+    public static void main(String[] args){
+        Construct solution = new Construct();
+
+        int[][] grid1 = {{0, 1}, {1, 0}};
+        int[][] grid2 = {
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 0, 0, 0}
+        };
+
+        Node root1 = solution.construct(grid1);
+        Node root2 = solution.construct(grid2);
+
+        System.out.println("Constructed for grid1");
+        System.out.println("Constructed for grid2");
+    }
 
 }
 
@@ -12,6 +63,7 @@ public class Construct {
  * - a tree in which each node has 4 children (topLeft. topRight, bottomLeft,
  * bottomRight)
  * - each node represents a rectangle in the plane
+ * - Taking help from gpt
  * 
  * Pattern :
  * 
