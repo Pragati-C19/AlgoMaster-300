12c74d596fe29b9edbf37b2e79fbfac04af1c2ef
Pragati Chothe <pragatichothe@gmail.com>
2025-03-20 17:11:24 +0530

    ⚡️Step 3: Code Writing

--- Divide & Conquer/Construct.java
+++ Divide & Conquer/Construct.java
@@ -1,6 +1,8 @@
 import java.util.*;;
 
 public class Construct {
+
+    // Node class representing each node in the Quad Tree
     private class Node {
         public boolean val;
         public boolean isLeaf;
@@ -8,14 +10,15 @@ public class Construct {
         public Node topRight;
         public Node bottomLeft;
         public Node bottomRight;
-    
-        public Node() {}
-    
+
+        public Node() {
+        }
+
         public Node(boolean _val, boolean _isLeaf) {
             val = _val;
             isLeaf = _isLeaf;
         }
-    
+
         public Node(boolean _val, boolean _isLeaf, Node _topLeft, Node _topRight, Node _bottomLeft, Node _bottomRight) {
             val = _val;
             isLeaf = _isLeaf;
@@ -24,26 +27,73 @@ public class Construct {
             bottomLeft = _bottomLeft;
             bottomRight = _bottomRight;
         }
-    } 
-
-    public Node construct(int[][] grid) {
-        
-        return 1;
     }
 
-    public static void main(String[] args){
+    // Main function to start the Quad Tree construction
+    public Node construct(int[][] grid) {
+        int n = grid.length;
+        return build(grid, 0, 0, n);
+    }
+
+    // Recursive function to build the Quad Tree
+    private Node build(int[][] grid, int row, int col, int size) {
+        System.out.println("Building node from row: " + row + ", col: " + col + ", size: " + size);
+
+        // If grid has same value, return a leaf node
+        if (isUniform(grid, row, col, size)) {
+            System.out.println("Leaf node created with value: " + grid[row][col]);
+            return new Node(grid[row][col] == 1, true);
+        }
+
+        // Divide into 4 quadrants (top-left, top-right, bottom-left, bottom-right)
+        int half = size / 2;
+        System.out.println("Splitting into quadrants...");
+
+        Node topLeft = build(grid, row, col, half);
+        Node topRight = build(grid, row, col + half, half);
+        Node bottomLeft = build(grid, row + half, col, half);
+        Node bottomRight = build(grid, row + half, col + half, half);
+
+        // If all children are leaf nodes with same value, merge them into one leaf node
+        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf &&
+                topLeft.val == topRight.val && topLeft.val == bottomLeft.val && topLeft.val == bottomRight.val) {
+
+            System.out.println("Merging leaf nodes into one leaf node with value: " + topLeft.val);
+            return new Node(topLeft.val, true);
+        }
+
+        // If they are different, create a parent node with these children
+        System.out.println("Creating a parent node with 4 children.");
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    // Check if the current grid section has all the same values
+    private boolean isUniform(int[][] grid, int row, int col, int size) {
+        int value = grid[row][col];
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
         Construct solution = new Construct();
 
-        int[][] grid1 = {{0, 1}, {1, 0}};
+        int[][] grid1 = { { 0, 1 }, { 1, 0 } };
         int[][] grid2 = {
-            {1, 1, 1, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 0, 0, 0}
+                { 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 1, 1, 1, 0, 0, 0, 0 }
         };
 
         Node root1 = solution.construct(grid1);
