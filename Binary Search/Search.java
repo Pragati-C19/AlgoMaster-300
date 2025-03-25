f59c7fdd8f70c5d0f78b393ff565c209c8122058
Pragati Chothe <pragatichothe@gmail.com>
2025-03-25 15:16:18 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | only using Brute force rn

---
+++ Binary Search/Search.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Search {
+    
+    
+}
+
+/*
+ * 
+ * Intuitions : 
+ * 
+ * 1. I think it's just checking the nums[i] == target and return i
+ * 2. otherwise return -1
+ * 
+ * 
+ * Pattern :
+ * 
+ * 
+ * 
+ * Pseudo Code : 
+ * 
+ * 1. Brute force:
+ * 
+ * function search(nums, target){
+ *     
+ * if(nums.length == 0) return -1;
+ * 
+ * for( int i = 0; i < nums.length - 1; i++){
+ *      if(nums[i] == target){
+ *          return i;
+ *      }
+ * }
+ * 
+ * return -1;
+ * 
+ * }
+ * 
+ * 
+ */
