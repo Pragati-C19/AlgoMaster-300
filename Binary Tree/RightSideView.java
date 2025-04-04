5c7a012170a970fd9b45f80f2d12c647334111ff
Pragati Chothe <pragatichothe@gmail.com>
2025-04-04 15:28:15 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this we don't need to add in leetcode

--- Binary Tree/RightSideView.java
+++ Binary Tree/RightSideView.java
@@ -1,7 +1,90 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class RightSideView {
     
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Driver Function
+    public List<Integer> rightSideView(TreeNode root) {
+        
+    }
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        // First Example
+        Integer[] treeArray1 = {1, 2, 3, null, 5, null, 4};
+        TreeNode root1 = buildTree(treeArray1);
+        
+        List<List<Integer>> result1 = levelOrder(root1);
+        System.out.println("Result1: " + result1 + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = {1, 2, 3, 4, null, null, null, 5};
+        TreeNode root2 = buildTree(treeArray2);
+        
+        List<List<Integer>> result2 = levelOrder(root2);
+        System.out.println("Result2: " + result2 + "\n");
+
+        // Third Example
+        Integer[] treeArray3 = {1, null, 3};
+        TreeNode root3 = buildTree(treeArray3);
+        
+        List<List<Integer>> result3 = levelOrder(root3);
+        System.out.println("Result3: " + result3);
+
+        // Fourth Example
+        Integer[] treeArray4 = {1, null, 3};
+        TreeNode root4 = buildTree(treeArray4);
+        
+        List<List<Integer>> result4 = levelOrder(root4);
+        System.out.println("Result4: " + result4);
+    }
 }
 
 
