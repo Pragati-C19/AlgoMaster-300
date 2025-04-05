a7cbd67f9558c47279d1660ee15e95c83ff540c4
Pragati Chothe <pragatichothe@gmail.com>
2025-04-05 15:11:39 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree/Connect.java
+++ Binary Tree/Connect.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class Connect {
 
     // Definition for a Node.
@@ -22,6 +27,60 @@ public class Connect {
         }
     };
 
+    // Driver Function
+    public Node connect(Node root) {
+        
+    }
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        Connect solution = new Connect();
+
+        // First Example
+        Integer[] treeArray1 = {1, 2, 3, 4, 5, null, 7};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.connect(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = {};
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.connect(root2) + "\n");
+
+    }
 
 
 }
