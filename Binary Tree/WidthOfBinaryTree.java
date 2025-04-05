7aa33e652c309ccdd85a3cd8fbfcbc89d989db36
Pragati Chothe <pragatichothe@gmail.com>
2025-04-05 17:32:12 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree/WidthOfBinaryTree.java
+++ Binary Tree/WidthOfBinaryTree.java
@@ -1,7 +1,82 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class WidthOfBinaryTree {
 
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Driver Function 
+    public int widthOfBinaryTree(TreeNode root) {
+        
+        return 0;
+    }
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        WidthOfBinaryTree solution = new WidthOfBinaryTree();
+
+        // First Example
+        Integer[] treeArray1 = {1, 3, 2, 5, 3, null, 9};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.widthOfBinaryTree(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = {1, 3, 2, 5, null, null, 9, 6, null, 7};
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.widthOfBinaryTree(root2) + "\n");
+
+        // Third Example
+        Integer[] treeArray3 = {1, 3, 2, 5};
+        TreeNode root3 = buildTree(treeArray3);
+        System.out.println("Result3: " + solution.widthOfBinaryTree(root3) + "\n");
+        
+    }
 }
 
 /*
