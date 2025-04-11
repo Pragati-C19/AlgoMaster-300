f9d91699d810699f52f263b8aba2e3761051aafc
Pragati Chothe <pragatichothe@gmail.com>
2025-04-11 16:14:22 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree - PreOrder/Codec.java
+++ Binary Tree - PreOrder/Codec.java
@@ -1,6 +1,76 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class Codec {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        
+    }
+
+
+    // Helper Function : To Print Binary Tree 
+    public static List<String> printTreeAsArrayFormat(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+    
+        queue.add(root);
+    
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+    
+            if (node == null) {
+                result.add("null");
+            } else {
+                result.add(String.valueOf(node.val));
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+    
+        // Trim trailing "null"s (Leetcode does this)
+        int i = result.size() - 1;
+        while (i >= 0 && result.get(i).equals("null")) {
+            result.remove(i--);
+        }
+    
+        return result;
+    }    
+
+    public static void main (String[] args){
+
+        Codec ser = new Codec();
+        Codec deser = new Codec();
+
+        TreeNode root1 = {1, 2, 3, null, null, 4, 5};
+        TreeNode ans1 = deser.deserialize(ser.serialize(root1))
+        System.out.println("Output1 : " + printTreeAsArrayFormat(ans1) + "\n");
+
+        TreeNode root2 = { };
+        TreeNode ans2 = deser.deserialize(ser.serialize(root2))
+        System.out.println("Output2 : " + printTreeAsArrayFormat(ans2) + "\n");
+
+    }
 
 }
 
