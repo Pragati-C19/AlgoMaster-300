0e14c26b8cbdfcce224aef7100839f3f3663b42d
Pragati Chothe <pragatichothe@gmail.com>
2025-04-13 16:47:06 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree - InOrder/GetMinimumDifference.java
+++ Binary Tree - InOrder/GetMinimumDifference.java
@@ -1,7 +1,83 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class GetMinimumDifference {
     
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Global Declaration of variables 
+    
+    // Driver function 
+    public int getMinimumDifference(TreeNode root) {
+        
+    }
+
+    // Recursion Function : To sort BST and get MinDiff
+    private void inorderTraversal(TreeNode root){
+
+    }
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        MinDiffInBST solution = new MinDiffInBST();
+
+        // First Example
+        Integer[] treeArray1 = {4, 2, 6, 1, 3};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.minDiffInBST(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = { 1, 0, 48, null, null, 12, 49 };
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.minDiffInBST(root2) + "\n");
+
+    }
 }
 
 /*
