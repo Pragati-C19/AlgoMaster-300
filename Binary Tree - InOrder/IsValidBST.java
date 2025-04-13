f507867d4fdd2b0346ecf88d5ed3c1db27d522b3
Pragati Chothe <pragatichothe@gmail.com>
2025-04-13 19:27:40 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | had a catch in 2nd approach too so created the new appraoch

--- Binary Tree - InOrder/IsValidBST.java
+++ Binary Tree - InOrder/IsValidBST.java
@@ -27,14 +27,6 @@ public class IsValidBST {
  * 4. return false
  * 5. problem in this is.. it will return true for every triplates which I don't want
  * 
- * 2nd Approach :
- * 1. Let's take global variable or let's check if condition in inorder function
- * 
- * 
- * Pseudo Code :
- * 
- * 1st Approach :
- * 
  * function isValidBST(root){
  * 
  *      List<Integer> arr = new Array
@@ -52,9 +44,10 @@ public class IsValidBST {
  * 
  * 
  * 2nd Approach :
+ * 1. Let's take global variable or let's check if condition in inorder function
+ * 2. ohk in my second approach there is problem over what if node.left and node.right is null?
+ * 3. it will automatically give error there.. 
  * 
- * prevNode = null;
- * nextNode = null;
  * boolean isValid
  * 
  * function isValidBST(root){
@@ -76,6 +69,70 @@ public class IsValidBST {
  *      inorder(node.right)
  * 
  * }
+ * 
+ * 
+ * 3rd Approach :
+ * 
+ * 1. will go with global variable approach where will add variables as
+ * - prevNode = null
+ * - nextNode = null
+ * - isValid = true
+ * 2. will call recursion function inorderTraversal
+ * - if(node == null) return 
+ * - call recursion to check left side
+ * - visit node : as in check if prevNode < node < nextNode also prevNode != null and nextNode != null
+ * -> if yes then return isValid = true
+ * -> if no return false
+ * - prevNode = node.val
+ * - nextNode = node.right.val 
+ * - call recursion to check right side
+ * 3. return isValid
+ * 
+ * 
+ * prevNode = null;
+ * nextNode = null;
+ * boolean isValid = true;
+ * 
+ * function isValidBST(root){
+ *      inorder(root)
+ *      return isValid
+ * }
+ * 
+ * function inorder(node){
+ * 
+ *      // Base Case
+ *      if(node == null) return
+ * 
+ *      // check left node
+ *      inorder(node.left)
+ * 
+ *      // Visit Node : check if condition is valid or not
+ *      if(prevNode != null && nextNode != null) {
+ *          if prevNode < node.val && node.val < nextNode){
+ *              isValid = true
+ *          }
+ *          else {
+ *              isValid = false
+ *          }
+ *      } 
+ *      prevNode = node.val
+ *      nextNode = node.right.val
+ * 
+ *      inorder(node.right)
+ * 
+ * }
+ * 
+ * Pseudo Code :
+ * 
+ * 1st Approach :
+ * 
+ * 
+ * 
+ * 2nd Approach :
+ * 
+ * prevNode = null;
+ * nextNode = null;
+ * 
  * 
  * 
  */
