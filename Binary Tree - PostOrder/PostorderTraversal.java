19baf337e9eaefaaf9e0fafc76dc58ed76e424f0
Pragati Chothe <pragatichothe@gmail.com>
2025-04-14 18:20:38 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree - PostOrder/PostorderTraversal.java
+++ Binary Tree - PostOrder/PostorderTraversal.java
@@ -1,7 +1,93 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class PostorderTraversal {
     
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    // Driver Function 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+    }
+
+    // Recursion Function : write actual code of post order
+    private void postOrder(TreeNode root, List<Integer> result){
+
+    }
+
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        PostorderTraversal solution = new PostorderTraversal();
+
+        // First Example
+        Integer[] treeArray1 = {1, null, 2, 3};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.postorderTraversal(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = { 1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9 };
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.postorderTraversal(root2) + "\n");
+
+        // Third Example
+        Integer[] treeArray3 = {1};
+        TreeNode root3 = buildTree(treeArray3);
+        System.out.println("Result3: " + solution.postorderTraversal(root3) + "\n");
+
+        // Forth Example
+        Integer[] treeArray4 = {};
+        TreeNode root4 = buildTree(treeArray4);
+        System.out.println("Result4: " + solution.postorderTraversal(root4) + "\n");
+
+    }
 }
 
 /*
