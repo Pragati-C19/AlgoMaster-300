51e349a860eb27d551ad76ed1ca1af138779ea27
Pragati Chothe <pragatichothe@gmail.com>
2025-04-14 19:48:50 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree - PostOrder/DiameterOfBinaryTree.java
+++ Binary Tree - PostOrder/DiameterOfBinaryTree.java
@@ -2,6 +2,91 @@ import java.util.*;
 
 public class DiameterOfBinaryTree {
     
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    // Driver Function 
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+    }
+
+    // Recursion Function : to check all paths
+    private int postOrder(TreeNode root, int diameter, List<Integer> currPath){
+
+    }
+
+    // Backtrack Function : to get longest Path
+    private void postOrder(TreeNode root, List<Integer> currPath){
+
+    }
+
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        DiameterOfBinaryTree solution = new DiameterOfBinaryTree();
+
+        // First Example
+        Integer[] treeArray1 = {1, 2, 3, 4, 5};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.diameterOfBinaryTree(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = { 1, 2 };
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.diameterOfBinaryTree(root2) + "\n");
+
+        // Third Example
+        Integer[] treeArray3 = { 4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2};
+        TreeNode root3 = buildTree(treeArray3);
+        System.out.println("Result3: " + solution.diameterOfBinaryTree(root3) + "\n");
+        
+    }
+
 }
 
 /*
