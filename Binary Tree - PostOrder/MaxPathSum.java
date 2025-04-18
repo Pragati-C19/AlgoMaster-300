219894b183f3b39036961deab8c63330b4ccfd2d
Pragati Chothe <pragatichothe@gmail.com>
2025-04-18 20:20:23 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Binary Tree - PostOrder/MaxPathSum.java
+++ Binary Tree - PostOrder/MaxPathSum.java
@@ -1,7 +1,84 @@
 import java.util.*;
 
+import javax.swing.tree.TreeNode;
+
 public class MaxPathSum {
     
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Globally Declare Variable
+
+
+    // Driver Function 
+    public int maxPathSum(TreeNode root) {
+        
+    }
+
+    // Recursion Function : to check sum of paths
+    private int postOrder(TreeNode node){
+
+    }
+
+    // Helper Function : to build a tree from an array (for testing)
+    public static TreeNode buildTree(Integer[] nodes) {
+        
+        // Node is empty
+        if (nodes.length == 0 || nodes[0] == null) return null;
+        
+        TreeNode root = new TreeNode(nodes[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        int i = 1;  // Start from second element
+
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode parent = queue.poll();
+            
+            // Assign left child
+            if (nodes[i] != null) {
+                parent.left = new TreeNode(nodes[i]);
+                queue.add(parent.left);
+            }
+            i++;
+            
+            // Assign right child (check if there's still an element)
+            if (i < nodes.length && nodes[i] != null) {
+                parent.right = new TreeNode(nodes[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        
+        MaxPathSum solution = new MaxPathSum();
+
+        // First Example
+        Integer[] treeArray1 = {1, 2, 3};
+        TreeNode root1 = buildTree(treeArray1);
+        System.out.println("Result1: " + solution.maxPathSum(root1) + "\n");
+
+        // Second Example
+        Integer[] treeArray2 = {-10, 9, 20, null, null, 15, 7};
+        TreeNode root2 = buildTree(treeArray2);
+        System.out.println("Result2: " + solution.maxPathSum(root2) + "\n");
+    
+    }
 }
 
 /*
