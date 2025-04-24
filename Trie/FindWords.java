12532702649fcf6fab26f97646b3350c78df8a65
Pragati Chothe <pragatichothe@gmail.com>
2025-04-24 23:55:54 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote a whole code seems simple noww..

--- Trie/FindWords.java
+++ Trie/FindWords.java
@@ -121,13 +121,31 @@ public class FindWords {
  *      - if it's there then again check next charecter of that word is present near that block
  * 3. Jr board vr same letters astil and suppose u are checking for eat and tula e bhetla but a tyachya aspass nahiye tr look for another e
  * 4. Let's check exist (Word Search) Problem first then will solve this one.. 
- * 5. let's dive into this
+ * 
  * 
  * Pattern :
  * 
- * 1. Wrote exact same code as I wrote for search Word.. just added one for loop [word : words]
+ * 1. Using Trie with DFS
+ * 2. will create a Trie struct first which will store child and word at the end 
+ * 3. wordStore will work just like isEndOfWord bool.. just now we are storing the whole word at that node
+ * 4. after insearting all words in trie
+ * 5. will traverse matrix as we did in word search que
+ * 6. will call dfs in it 
+ * 7. our base case to get result will be checking (word != null) or (isEndOfWord = true) 
+ * 8. why this?... bcoz for example u are storing oath in trie
+ *         o
+ *         ├─ a
+ *         │  ├─ t ─ h 
+ *    
+ *    only at h word is stored as oath and isEndOfWord will be true 
+ *    other char it will be null and false
+ * 9. after checking word != null if yes then add that word in result and change wordStore at that node as null
+ * so that u'll not get duplicates (u can use set too to check if block is visited or not)
+ * 10. if that charecter at block is in current TrieNode -> then check other sides
  * 
- * Second appraoch Using Trie :-
+ * 
+ * 
+ * Appraoch Using Trie :-
  * 
  * 1. Create TrieNode Struct :
  *      - String wordStored -> store complete word here when it's the end
@@ -135,21 +153,95 @@ public class FindWords {
  *      - isEndOfWord -> checks if any word is ending at this char?
  * 2. Globally declare root -> initial stage of any TrieNode
  * 3. Declare a result List to store String
- * 4. Declare foundWords hashSet to avoid duplicates
- * 5. focus on insearting all words in the Trie
+ * 4. focus on insearting all words in the Trie
  *      -> for(word : words) -> inseart(word)
  *          Traverse through each charecter of that word
  *          create a node if missing
  *          at the end make isEndOfWord = true and wordStored = word
  * 5. Start traversing matrix use for loop here
  *      -> call dfs recursion
- *          Base Cases : 0 < i, j >= board.length || visitedBlock -> return  
- *          If isEndOfWord = true and word is not in set -> add that word to set and and result return
+ *          Base Cases : 0 > i, j >= board.length || visitedBlock -> return  
+ *          If isEndOfWord = true and wordStored != null -> add that to result and wordStored = null return
  *          check if char at board[i][j] is present in TrieNode? -> if yes then check (down, right, up, left) 
  * 6. return result
  * 
+ * 
+ * 
  * Pseudo Code :
  * 
+ * 
+ * TrieNode {
+ *      child = TrieNode[26]
+ *      wordStored = null
+ *      isEndOfWord = false
+ * }
+ * 
+ * Globally declare root and result
+ * 
+ * function findWords(board, words){
+ * 
+ *      -> get length of row and col (m x n)
+ * 
+ *      -> assign value to root and result
+ * 
+ *      -> Visited -> to check if block is visited or not
+ * 
+ *      -> for(word : words) -> call function insert(word)
+ * 
+ *      -> for(i = 0 < m) inside for( j = 0 < n)
+ *          call recursion function dfs(i, j, root, visited, board, m, n)
+ * 
+ *      -> return result
+ *      
+ * }
+ * 
+ * 
+ * function insert(word){
+ * 
+ *      -> node = root;
+ *      
+ *      -> for(ch : word)
+ *              index = ch - 'a'
+ *              if(node.child[index] == null){
+ *                      node.child[index] = new TrieNode()
+ *              }
+ *              node = node.child[index]
+ *      
+ *      -> update node.word and isEndOfWord
+ *              node.word = word
+ *              node.isEndOfWord = true
+ * 
+ * }
+ * 
+ * 
+ * function dfs( i, j, node, visited, board, m, n){
+ *  
+ *      -> Base Case :
+ *      if(0 > i, j >= m , n || visited) -> return 
+ * 
+ *      -> Get result
+ *      if(word != null) 
+ *          result.add(word) 
+ *          word = null
+ * 
+ *      -> Mark this block as isited
+ *          visited = true
+ *  
+ *      -> Checking if charecter at bloack exist in Trie
+ *      
+ *      ch = board[i][j]
+ *      indexOfChar = ch - 'a'
+ *      
+ *      if(node.child[indexOfChar] != null){
+ *          dfs(i+1, j, node.child[indexOfChar], visited, board, m, n) -> down
+ *          dfs(i, j+1, node.child[indexOfChar], visited, board, m, n) -> right
+ *          dfs(i-1, j, node.child[indexOfChar], visited, board, m, n) -> up
+ *          dfs(i, j-1, node.child[indexOfChar], visited, board, m, n) -> left
+ *      }
+ * 
+ *      visited = false
+ *      return
+ * }
  * 
  * 
  * 
