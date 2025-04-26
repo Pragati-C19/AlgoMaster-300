a75b0660401f5f6048ab223cbbf374053d579a52
Pragati Chothe <pragatichothe@gmail.com>
2025-04-26 20:13:35 +0530

    🐛 Debugging 2 | Wrote proper understanging

--- Heap(Priority Queue)/Two Heap/MedianFinder.java
+++ Heap(Priority Queue)/Two Heap/MedianFinder.java
@@ -150,7 +150,8 @@ public class MedianFinder {
  * 3. findMedian() 
  *      - if(arr.length == 1) -> return arr[0]
  *      - if(arr.length == 2) -> (arr[0] + arr[1]) / 2
- *      - Use Binary search to find median
+ *      - if even length then : ( (arr.length / 2) + ((arr.length / 2) - 1) ) / 2 
+ *      - if odd length then : arr.length / 2
  * 
  * Need to use Two heap as it specifically said in topic so
  * 
@@ -165,7 +166,59 @@ public class MedianFinder {
  *      - (minHeap.peek() + maxHeap.peek()) / 2
  * 
  * 
- * Pseudo Code :
+ * Understand the code written :
+ * 
+ * //? check out this video to understand better
+ * https://www.youtube.com/watch?v=jnj87BSi9Is&ab_channel=codestorywithMIK
+ * 
+ * 1. why use mean and max heap and why gave that value only?..
+ * 
+ * 2. take example [3, 4, 5, 8, 9, 10] this is a sorted even length array now to check median will only need 5 and 8 here
+ * -> how to get 5 or 8 easily?
+ *      - jr me maxHeap use kela and tyat left side che nums store kele till 5 tr mala easy jail 5 pop out karayla? 
+ *              maxHeap = [5, 4, 3]
+ *      - jr me minHeap use kela and tyat Right side che nums store kele from 8 to 10 tr mala easy jail 8 pop out karayla?
+ *              minHeap = [8, 9, 10]
+ * 
+ * 3. take example [3, 4, 5, 9, 10] this is a sorted odd length array now to check median will only need 5
+ * -> how to get 5 easily?
+ *      - jr odd number length ahe tr ek queue madhe jast elements astil ani eka yachya kami right?
+ *          like ekat 3 element ahe ekat 2
+ *      - let's assume ki maxHeap madhe ek element jast asel nehmi
+ *          like maxHeap = [5, 4, 3] and minHeap = [9, 10]
+ *      - so ata odd madhe kay karu?.. left side cha heap means maxHeap cha top element return karu
+ *      
+ * 4. take one final example with data streame
+ *  -> [0, 1, 3, 4, 5, 8, 9]
+ * 
+ *      step 1: [0]
+ *              maxHeap = [0]  minHeap = []
+ * 
+ *      step 2: [1]  -> 0 < 1
+ *              maxHeap = [0]  minHeap = [1]
+ * 
+ *      step 3: [3]  -> 0 < 3 so minHeap madhe 3 add kel
+ *              maxHeap = [0]  minHeap = [1, 3]
+ *         but apan bagh kay mhnt hoto ki maxHeap vala motha thevnar apa right?.. remember that second example?
+ *         tyamul kay kel minHeap cha top vapas maxHeap madhe takla 
+ *              maxHeap = [1, 0]  minHeap = [3]
+ * 
+ *      step 4: [4]  -> 1 < 4 
+ *              maxHeap = [1, 0]  minHeap = [3, 4]
+ * 
+ *      step 5: [5]  -> 1 < 5 
+ *              maxHeap = [1, 0]  minHeap = [3, 4, 5]
+ *          maxHeap chi size minHeap pekshya choti ahe so apan parat add karu minHeap cha top to maxHeap
+ *              maxHeap = [3, 1, 0]  minHeap = [4, 5]
+ * 
+ *      step 6: [8]  -> 3 < 8 
+ *              maxHeap = [3, 1, 0]  minHeap = [4, 5, 8]
+ * 
+ *      step 7: [9]  -> 3 < 9
+ *              maxHeap = [3, 1, 0]  minHeap = [4, 5, 8, 9]
+ *          maxHeap chi size minHeap pekshya choti ahe so apan parat add karu minHeap cha top to maxHeap
+ *              maxHeap = [4, 3, 1, 0]  minHeap = [5, 8, 9]
+ * 
  * 
  * 
  * 
