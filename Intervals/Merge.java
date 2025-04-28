e0a8651245418f12d512483e2b003a2b37fd8ba9
Pragati Chothe <pragatichothe@gmail.com>
2025-04-28 14:24:56 +0530

    🐛 Debugging 1 | first it gave me bound error.. now it's not taking other values as it is.. giving [0,0] there..

--- Intervals/Merge.java
+++ Intervals/Merge.java
@@ -11,7 +11,26 @@ public class Merge {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         System.out.println("  Sorted Array : " + Arrays.deepToString(intervals));
 
-        return intervals;
+        for (int i = 0; i < n - 1; i++) {
+
+            if (intervals[i+1][0] < intervals[i][1]) {
+                
+                System.out.println(" Intervals are start[next] " + intervals[i+1][0] + " < end[curr] " + intervals[i][1]);
+
+                result[i][0] = intervals[i][0];
+                result[i][1] = intervals[i+1][1];
+
+                if(intervals[i+1][1] < intervals[i][1]) {
+                    
+                    System.out.println(" Intervals are end[next] " + intervals[i+1][1] + " < end[curr] " + intervals[i][1]);
+                    
+                    result[i][1] = intervals[i][1];
+                }
+            }
+        
+        }
+
+        return result;
     }
 
     public static void main(String[] args){
