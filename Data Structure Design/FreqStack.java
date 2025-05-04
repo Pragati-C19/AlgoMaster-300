791ed66d958c908db18adf77744ec9cdbfc37027
Pragati Chothe <pragatichothe@gmail.com>
2025-05-04 18:05:24 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Thought abt the que so far.. but still feels something is off

---
+++ Data Structure Design/FreqStack.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class FreqStack {
+    
+}
+
+/*
+ * 
+ * 
+ * Intuitions :
+ * 
+ * 1. we need to create a frequncy stack
+ * 2. void push(int val) pushes an integer val onto the top of the stack.
+ * 3. int pop() removes and returns the most frequent element in the stack.
+ * 4. what is most freq element means?
+ *      int[] nums = {1, 3, 2, 3, 4, 3, 5};
+ *      Frequencies:
+ *          1 → 1 time
+ *          2 → 1 time
+ *          3 → 3 times ✅
+ *          4 → 1 time
+ *          5 → 1 time
+ *      Most frequent element = 3 (since it appears the most times)
+ * 
+ * 
+ * 
+ * Pattern :
+ * 
+ * 1. it's abt frequncy so my first thought is to use HashMap to store freq of that val
+ * 2. void Push(int val)
+ *      - will push val in stack
+ *      - then check karel if map.contains(val)
+ *          - if yes then increase it's frequency
+ *          - if no then add val in map with freq 1
+ * 3. int pop()
+ *      - get the high freq element from map
+ *      - how can you get it?
+ *      - will use for loop for map
+ *          for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+ *              if (entry.getValue() > maxFreq) {
+ *                  maxFreq = entry.getValue();
+ *                  element = entry.getKey();
+ *              }
+ *	        }
+ 
+ *      - I thought of using max directly but then getting key against that maxFreqVal will be hard
+ *              maxFreqVal = max(maxFreqVal, entry.getValue())
+ *      
+ *      - After getting max will it's time to pop
+ *          means will decrese it's freq count 
+ *          if count hits to 0 will remove that val from hashmap
+ * 
+ * 
+ * 
+ * Pseudo Code :
+ * 
+ * 
+ * 
+ */
