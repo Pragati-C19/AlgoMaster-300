29864ccf7053ce4cdc5ce471c0436850ca2d113b
Pragati Chothe <pragatichothe@gmail.com>
2025-05-04 18:45:14 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote a pseudo code now.. with two hashmaps.. I think now it will work

--- Data Structure Design/FreqStack.java
+++ Data Structure Design/FreqStack.java
@@ -26,6 +26,9 @@ public class FreqStack {
  * 
  * Pattern :
  * 
+ * 
+ * 1st Approach with one Hashmap and stack
+ * 
  * 1. it's abt frequncy so my first thought is to use HashMap to store freq of that val
  * 2. void Push(int val)
  *      - will push val in stack
@@ -42,7 +45,7 @@ public class FreqStack {
  *                  element = entry.getKey();
  *              }
  *	        }
- 
+
  *      - I thought of using max directly but then getting key against that maxFreqVal will be hard
  *              maxFreqVal = max(maxFreqVal, entry.getValue())
  *      
@@ -51,8 +54,53 @@ public class FreqStack {
  *          if count hits to 0 will remove that val from hashmap
  * 
  * 
+ * 2nd Approach with 2 Hashmaps
+ * 
+ * 1. Global Variables are
+ *      freqMap -> It will store freq of every value here
+ *      groupValByFreqMap -> It will stores freq as key and list of nums which has that freq as value
+ *      maxFreqCount -> it will track maximum count of frequncy so far
+ * 
+ * 2. push(val)
+ *      - Take freq from freqMap for that val
+ *      - I'm finding freq here if exist or adding by default 1 if not exist
+ *          freqOfVal = freqMap.getOrDefault(val, 0) + 1
+ *      - adding that freq to freqMap
+ *          freqMap.put(val, freqOfVal)
+ *      - check if that group of freq exist in groupValByFreqMap
+ *          if(!map.contains(freqOfVal))
+ *              map.put(freqOfVal, new Stack)
+ *          map.get(freqOfVal).push(val)
+ *      - check if this freqOfVal is greater than maxFreqCount
+ *          if yes -> then change maxFreqCount to freqOfVal
+ * 
+ * 
+ * 3. pop()
+ *      - pop will be easier now I just need to pop eleemnt from stack which has high Freq
+ *      - How to find highest freqency?
+ *          for (Map.Entry<Integer, Integer> entry : groupValByFreqMap.entrySet()) {
+ *              if (entry.getKey() > maxFreqCount) {
+ *                  maxFreqCount = entry.getKey();
+ *              }
+ *	        }
+ *      - Will store stack for that maxFreq in a variable for better access later
+ *          stack = groupValByFreqMap.get(maxFreqCount)
+ *      - pop out top element from that stack 
+ *          stack.pop()
+ *      - decrese the freq from freqMap
+ *          freqMap.put(val, freqMap.get(val) - 1);
+ *      - if(freqMap.get(val) == 0)
+ *          freqMap.remove(val)
+ *      - jr group map madhe pn stack empty zala tr mala delete karav lagel ti key
+ *      - If the stack for maxFreqCount becomes empty, remove stack from map and reduce maxFreqCount
+ *          if (groupValByFreqMap.get(maxFreqCount).isEmpty()) 
+ *              groupValByFreqMap.remove(maxFreqCount);   
+ *              maxFreqCount--;
+ *         
+ *  
  * 
  * Pseudo Code :
+ * 
  * 
  * 
  * 
