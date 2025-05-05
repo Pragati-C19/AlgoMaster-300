ed9efe1f2ec4e68872ee6e8e7840cf49ee5438ed
Pragati Chothe <pragatichothe@gmail.com>
2025-05-05 22:21:46 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I thought abt it for long time commiting after 2hrs maybe or more

---
+++ Greedy/MinAddToMakeValid.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class MinAddToMakeValid {
+    
+}
+
+/*
+ * 
+ * 
+ * Intuitions :
+ * 
+ * 1. Parentheses string is valid if below conditions are true
+ *      - if String is empty -> it's valid
+ *      - It can be written as (A concatenated with B), where A and B are valid strings
+ *      - It can be written as (A), where A is a valid string
+ * 2. tbh I don't understand last 2 points
+ * 3. u have given a parenthesis string
+ * 4. in one move, u can insert a parenthesis at any position of the string
+ *      - example : s = "()))"
+ *          u can add ( at any position -> like here I added at index 1 "(()))"
+ *          or ) at any position        -> like here I added at index 3 "())))"
+ * 5. return the number of moves required to make s valid
+ * 
+ * 
+ * Pattern :
+ * 
+ * 1. Trace example :
+ * 
+ *      example : ())
+ *      - so parentheses valid hoil jevha he string (()) ashi banel
+ *      - as in equal number of ( and ) in string asel tevha
+ *      - so ata ithe tr dusra kahi option nahiye valid karayla..
+ *      - for index 0 -> (()) , )())
+ *      - for index 1 -> (()) , ()))
+ *      - for index 2 -> ())) , ()()
+ * 
+ * 
+ * What I understand
+ * 
+ * 1. jr maza openParenthesisCount == closeParenthesisCount asel tr ti string valid ahe
+ * 2. count open and close parenthesis of a given string ())
+ *      open = 1 and close = 2
+ * 3. now say how many parenthesis we need to make it valid? 
+ *      2 - 1 = 1 open 
+ * 4. count that move and return it
+ * 
+ * 
+ * check second example too
+ *        String = (((
+ *          open = 3 and close = 0
+ *          diff = 3 - 0 = 3
+ *          return 3
+ *          
+ *      
+ * 
+ * 
+ * 
+ * Pseudo Code :
+ * 
+ * 
+ * 
+ */
