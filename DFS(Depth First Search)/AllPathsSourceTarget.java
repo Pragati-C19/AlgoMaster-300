e588e2ec3f004014cfd0d7aad034a649047fe17f
Pragati Chothe <pragatichothe@gmail.com>
2025-05-10 16:52:21 +0530

    🐛 Debugging 3 | Changed comment pattern a bit

---
+++ DFS(Depth First Search)/AllPathsSourceTarget.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class AllPathsSourceTarget {
+    
+}
+
+/*
+ * 
+ * Intuitions :
+ * 
+ * 1. We have given Directed Acyclic Graph (DAG) of n nodes
+ * 2. Find all possible paths from node 0 to node n - 1
+ * 3. return them in any order
+ * 4. graph array looks like : 
+ *      graph[i] is list of all nodes you can visit from node i
+ *      graph = [[1,2],[3],[3],[]]
+ *      - from node 0 we can visit node [1, 2]
+ *      - from node 1 we can visit node [3]
+ *      - from node 2 we can visit node [3]
+ *      - from node 3 we can't visit any node as the list is empty
+ * 
+ * 
+ * Pattern :
+ * 
+ * 
+ * 
+ * Pseudo Code :
+ * 
+ * 
+ * 
+ */
