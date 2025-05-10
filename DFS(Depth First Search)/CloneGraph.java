653bd985a50a926d00aceda7878d51b0e6804a82
Pragati Chothe <pragatichothe@gmail.com>
2025-05-10 20:10:30 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I have to think of two brain stuck things, 1st abt where I should use recur call in for loop, 2nd do I need to use backtrack?

---
+++ DFS(Depth First Search)/CloneGraph.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class CloneGraph {
+    
+}
+
+/*
+ 
+ *  Intutions :
+
+    1. We have given a of conneted nodes from the index i
+    2. we need to create a clone of that adjList (1-indexed)
+    3. each node in graph contains 
+        int val, list[node] neighbors
+    4. node's val is same as index
+    5. first node will always be val = 1 bcoz adjList is a (1-indexed)
+    6. few conditions to know :
+        - you can't return the same graph -> mhnje jo input ahe tyalach output mhanna
+        - If nodes are cloned they should have same connections
+        - graph is cloned when nodes are new, and it looks exactly like the given graph
+
+
+ *  Pattern :
+
+    1. we need to use dfs -> to clone the nodes and it's neighbors
+    2. will maintain a visited list/set to avoid going in infinite loop or getting any duplicates
+    3. globally declare variables
+        - clonedNode
+        
+    4. function
+        - Declare a set to maintain visited Nodes
+            visitedSet = new HashSet
+
+    5. call Recursion function : (currNode)
+        - If node is already visited will skip to add it again 
+            if(visitedNode.contains(currNode))
+                return;
+
+        - If node is not visited will add it in set 
+            visitedNode.add(currNode)
+
+        - will create a new node of value currNode -> we are cloning the currNode
+            clonedNode = new Node(currNode.val)
+        
+        - to clone neighbors 
+          currNode.neighbors -> will give list<integer> of all neighbors of currNode
+            for(neighborNode : currNode.neighbors)
+        
+        - to add neighbor in clonedNode
+            clonedNode.neighbors.add(neighborNode)
+
+        - Where will we call recursion again -> //! This I need to imporve recursion call in recursion 
+            dfs(neighborNode)
+
+        - Backtrack currNode after adding it in clonedNode
+            Do I really need to use backtrack?
+
+
+
+
+
+ *  Pseudo Code :
+
+
+
+ */
