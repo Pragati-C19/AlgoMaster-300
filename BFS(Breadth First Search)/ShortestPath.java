6355edeb6ac249acbb29b0053c42132f67a4dea5
Pragati Chothe <pragatichothe@gmail.com>
2025-05-16 17:06:11 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | clear my thoughts and wrote pseudo code

--- BFS(Breadth First Search)/ShortestPath.java
+++ BFS(Breadth First Search)/ShortestPath.java
@@ -17,7 +17,7 @@ public class ShortestPath {
     https://www.youtube.com/watch?v=zpOhlTndBv0&ab_channel=codestorywithMIK
 
 
-    
+
  * Intuitions :
 
     1. we have given a matrix with size m x n
@@ -30,6 +30,7 @@ public class ShortestPath {
 
  * Pattern :
 
+ Appraoch 1:
     1. we are using BFS
     2. create a queue and initially store (0, 0) in it
     3. mark visited that (0, 0) cell 
@@ -42,18 +43,109 @@ public class ShortestPath {
             define x, y
             if(Base Case && visited != true)
 
-                todo: How to destroy obstacles and how to manage it I'm not sure
+                ? How to destroy obstacles and how to manage it I'm not sure
                 queue.add(x, y)
                 visited[x][y] = true
         - path++
     5. return path
 
 
+ Appraoch 2:
+    
+    1. we can visit any cell with different k_remain (obstacles remain) value
+    2. see in below example we have given a matrix and gave k = 1
+        - to visit (1, 2) from (0, 0) we can go with 2 paths 
+        - path 1 : (0, 0) -> (0, 1) -> (0, 2) -> (1, 2) : k_remain = 1
+        - path 2 : (0, 0) -> (0, 1) -> (1, 1) (obstacle) -> (1, 2) : k_remain = 0
+               
+                0 -> 0 -> 0                 0 -> 0    0
+                          |                      |
+                1    1    0                 1    1 -> 0
+                0    0    0                 0    0    0
+                0    1    1                 0    1    1
+                0    0    0                 0    0    0
+
+                   path1                       path2
+
+        - if u see there is one more obstacle at (3, 2) 
+            if we go with path 2 and mark only cell as visited then it can be possible that will skip path 1 fully
+            but if we mention visited as path with remaining k value it can revisit those cells many times but with different k remaining
+            to avoid any nonvisited path
+
+    3. so will do same as above just add obstacles remain in queue and visited
+        - use for loop to check other BRUL direction of that popout cells
+            define x, y
+            if(Base Case && visited != true)
+
+                ? How to destroy obstacles and how to manage it I'm not sure
+                queue.add(x, y)
+                visited[x][y] = true
+
+        - now this above steps looks like below
+            use for loop to check other BRUL direction of that popout cells
+            define x, y
+            if(Base Case)
+                if(we found grid[x][y] == 1) means obstacle found
+                    obstaclesRemain = k_Remain - 1
+                else : 
+                    obstacleRemain = k_Remain
+                
+                if(obstacleRemain >= 0 && cell is not visited with that k_Remain)
+                    queue.add(x, y, obstacleRemain)
+                    visited.add(x, y, obstacleRemain)
+        
+        - k_remain and obstacleRemain are different hun.. 
+            - k_Remain is of pop up cell's
+            - obstacleRemain is for the direction we are visiting from that cell
+                
+
+
  * Pseudo Code :
 
+    fuction shortestPath (grid, k) {
+    
+        -> Declare variables
+            m, n
+            matrixDirection
+            steps
+            queue<int[]>        - to store i, j, obstacleRemain 
+            visited[m][n][k]    - to store i, j, obstacleRemain
+            
+        -> Initially add (0, 0, k) in queue and visited
+        
+        -> BFS now
 
+            while(!queue.isEmpty)
+                queueSize = queue.size
 
+                -> start level for(i = 0 to queueSize)
 
+                    -> pop out top element in queue
+                    
+                    -> popcell[0] = row, popcell[1] = col, popcell[2] = obstacleRemainAtpopUpCell
+
+                    -> if (popcell[0] == m - 1 && popcell[1] == n - 1)
+                        return steps
+
+                    -> check that cells BRUL
+                        for(dir : matrixDirection)
+
+                            x = row + dir[0]  y = col + dir[1]
+
+                            if(grid[x][y] == 1)
+                                obstacleRemain = popcell[2] + 1
+                            else 
+                                obstacleRemain = popcell[2]
+                        
+                            if(visited != true && obstacleRemain >= 0 )
+                                queue.add(x, y, obstacleRemain)
+                                visited.add(x, y, obstacleRemain)
+                        
+                steps++
+
+        return -1
+    
+    }
 
 
  */
