eece04e9d6ae09c6da49f2920c549536dc5b37f1
Pragati Chothe <pragatichothe@gmail.com>
2025-05-20 18:10:03 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | ohk I think this should work

--- Topological Sorting/FindMinHeightTrees.java
+++ Topological Sorting/FindMinHeightTrees.java
@@ -101,8 +101,100 @@ public class FindMinHeightTrees {
         - I think when I say this is root I need to add values in map according to that node?
         - I'm only stuck at here
 
+^   Solution to my cycle detection problem
+    - see maza problem hota jr me grpah map madhe store kelya value tr tya distil like below
+            0 - 3
+            1 - 3
+            2 - 3
+            3 - 0, 1, 2, 4
+            4 - 3, 5
+            5 - 4
+    - jr me root as 0 mhnle and jevha te 3 la check karayla jail tevha mhnelch ki ohh u need to visit 0 first cycle detected
+    - tyamul to avoid this.. apan rk kam karu currNode, visitingState sobt apan parentNode pn deu
+    - will check if neighbor == parentNode then continue
+
+
  
  * Pseudo Code :
 
+    findMinHeightTrees (n, edge) {
+    
+        -> Declare variables
+            graphMap = new Hashmap
+            visitingState = new int[n]      -> there are n nodes
+            minHeight = Max_Value
+            heightNodeMap                   -> to store height as key and list of nodes as value
+            result
+
+        -> add dependencies in grpahmap
+            it's a little bit different bcoz [1, 0] means 1 -> 0 and 0 -> 1
+            length of edge[i] is 2 bcoz it as (a, b)
+            
+            for(i = 0 to edge.length)
+
+                -> check for 0'th index 
+                    if(!map.contains(edge[i][0]))
+                        map.put(edge[i][0], ArrayList)
+
+                -> check for 1'st index
+                    if(!map.contains(edge[i][1]))
+                        map.put(edge[i][1], ArrayList)
+
+                -> now add values in array
+                    map.get(edge[i][0]).add(edge[i][1])
+                    map.get(edge[i][1]).add(edge[i][0])
+
+        
+        -> now call dfs 
+            for(rootNode = 0 to n)
+
+                - get curr height
+                currHeight = dfs(rootNode, visitingState, rootNode, 0)
+
+                - add that height and node in map
+                    if(!heightNodeMap.contains(currHeight))
+                        heightNodeMap.put(currHeight, new ArrayList)
+
+                    heightNodeMap.get(currHeight).add(rootNode)
+                        
+                - update minHeight value
+                minHeight = min(currHeight, minHeight)
+
+        -> add nodes with minHeight in result
+            if(heightNodeMap.contains(minHeight))
+                
+                listOfNode = map.get(minHeight)
+                
+                for(i = 0 to listOfNode.lenght)
+                    result.add(listOfNode.get(i))
+
+        -> return result
+
+    }
+
+
+    dfs (currNode, visitingState, parentNode, currHeight) {
+    
+        -> Base Case :
+            if(visitingState[currNode] == 2)    return currHeight
+
+        -> mark currNode as visiting
+            visitingState[currNode] = 1
+        
+
+        -> check for neighbors 
+            for(neighbor : map.get(currNode))
+
+                -> to manage cycle detection for this problem 
+                    if(neighbor == parentNode) continue
+                    
+                dfs(neighbor, visitingState, currNode, currHeight + 1)
+
+        -> mark currNode as visited now as we check it's neighbors so
+            visitingState[currNode] = 2
+        
+        -> if everything works we can return as currHeight
+    
+    }
 
  */
