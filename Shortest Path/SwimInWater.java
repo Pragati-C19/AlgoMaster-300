7e63b5ea7de5debbe4fc8aa8969ebcaf9739268c
Pragati Chothe <pragatichothe@gmail.com>
2025-05-25 22:22:07 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote logic on my own

---
+++ Shortest Path/SwimInWater.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class SwimInWater {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have given an matrix grid with size n x n
+        - where each value grid[i][j] represents the elevation at that point (i, j)
+    2. Rain starts to fall at time t
+    3. You can swim in BRUL directions
+    4. return the least time until we can reach the bottom right square 
+
+ 
+ * Pattern :
+ 
+    1. We need an minHeap will store {row, col, timeTakenSoFar}
+    2. we need bestTimeTaken array to store minimun time it takes to reach that cell
+    3. we need matrixDirection and visitedCell array 
+    4. add initial values in minHeap
+        - {0, 0, 0}
+    5. start while loop till minHeap get's empty
+        - Pop out top
+            currRow, currCol, currTimeTaken
+        - if we have reach to the end of matrix
+            return currTimeTaken
+        - mark cell as visited
+
+    6. Check neighbors
+        - check time needed to jump on this neighbor cell
+            timeNeededSoFar = grid[neighborRow][neighborCol] - grid[currRow][currCol]
+
+        - if timeNeededSoFar > bestTimeTaken[neighborRow][neighborCol]
+            bestTimeTaken[neighborRow][neighborCol] = timeNeededSoFar
+            minHeap.add({neighborRow, neighborCol, timeNeededSoFar})
+
+    7. This que is soo similar as minimumEffortPath one.. 
+        - we want to check timeTaken jo diff ahe consecutive cells cha 
+        - jo maxTime asel tya path vrcha will add it in minHeap
+        - and to minHeap nehmi min timeTaken vali cell pop karat jail 
+ 
+ * Psuedo Code :
+ 
+
+
+ */
