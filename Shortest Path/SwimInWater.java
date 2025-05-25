2ea37244707a2b4bd300a7328222de666127aa01
Pragati Chothe <pragatichothe@gmail.com>
2025-05-25 22:24:23 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

--- Shortest Path/SwimInWater.java
+++ Shortest Path/SwimInWater.java
@@ -2,6 +2,32 @@ import java.util.*;
 
 public class SwimInWater {
     
+    public int swimInWater(int[][] grid) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+        SwimInWater solution = new SwimInWater();
+
+        int[][] grid1 = {
+            {0, 2},
+            {1, 3}
+        };
+        System.out.println("Result 1 -> " + solution.swimInWater(grid1) + "\n");   // Output: 3
+
+        int[][] grid2 = {
+            {0, 1, 2, 3, 4},
+            {24, 23, 22, 21, 5},
+            {12, 13, 14, 15, 16},
+            {11, 17, 18, 19, 20},
+            {10, 9, 8, 7, 6}
+        };
+        System.out.println("Result 2 -> " + solution.swimInWater(grid2) + "\n");   // Output: 16
+
+    }
+
 }
 
 /*
