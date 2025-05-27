35cbd81fc83dafdf45fe5d5402488cd3e521ca4a
Pragati Chothe <pragatichothe@gmail.com>
2025-05-27 14:36:37 +0530

    🌱 Step 1 : Basic setup added | Wrote basic code to run Binary Tree on VsCode, this won't be needed to add in leetcode

---
+++ 40. DP/1. 1-DP/ClimbStairs.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class ClimbStairs {
+    
+    public int climbStairs(int n) {
+     
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        ClimbStairs solution = new ClimbStairs();
+
+        int n1 = 2;
+        System.out.println("Result 1 -> " + solution.climbStairs(n1) + "\n");
+
+        int n2 = 3;
+        System.out.println("Result 2 -> " + solution.climbStairs(n2) + "\n");
+        
+    }
+
+}
+
+/*
+ * Intuitions :
+ 
+    1. 
+ 
+ 
+ * Pattern :
+ 
+
+ 
+ 
+ * Pseudo Code :
+ 
+
+
+
+ */
