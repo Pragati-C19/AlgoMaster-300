19c42ad59d589c22320c392626238cc413562f47
Pragati Chothe <pragatichothe@gmail.com>
2025-05-27 15:38:49 +0530

    📝 Step 2 : Intuition, Pattern or Pseudo code | Wrote logic on my own

--- 40. DP/1. 1-DP/ClimbStairs.java
+++ 40. DP/1. 1-DP/ClimbStairs.java
@@ -24,16 +24,41 @@ public class ClimbStairs {
 /*
  * Intuitions :
  
-    1. 
+    1. we have given n steps
+    2. and each time u can either climb 1 or 2 steps
+    3. we need to know how many distinct way we can able to climb to top
  
  
  * Pattern :
  
-
+    1. This problem is kind of fibonacci series
+    2. To reach step i:
+        - we could have come from step i - 1 (1 step)
+        - or from step i - 2 (2 steps)
+    3. so addition of it means we have come that much ways combined
+    4. try to solve this problem with DP appraoch 
  
  
  * Pseudo Code :
  
+    function climbStairs(n) {
+    
+        -> Declare a DP array of size n
+            will store ways to reach that step 
+
+        - 0th index will be the ground so there is only 1 way to go there
+        - 1st index means first step to go there we have only 1 way from oth index 
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for(i = 2 to n) 
+
+            dp[i] = dp[i-1] + dp[i-2]
+
+        -> why return dp[n] ? bcoz it's the highest count of ways possible to reach the top
+        return dp[n]
+    }
 
 
 
