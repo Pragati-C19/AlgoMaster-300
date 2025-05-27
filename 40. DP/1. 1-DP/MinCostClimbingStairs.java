9905d4162bdcb9c7cfc7b1724caa653838bd4e32
Pragati Chothe <pragatichothe@gmail.com>
2025-05-27 16:15:49 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote logic on my own

---
+++ 40. DP/1. 1-DP/MinCostClimbingStairs.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class MinCostClimbingStairs {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. we have given an array cost
+        where cost[i] is the cost of i'th step on staircase
+    2. once we pay the cost, we can climb 1 or 2 steps
+    3. we can start from index 0 or index 1
+    4. need to return minimum cost to reach the top of the floor
+
+ 
+ * Pattern :
+ 
+    1. declare a dp to check minimum cost we spend to reach the specific step
+    2. dp[0] which is ground so it's cost is same as 0'th index cost
+    3. dp[1] which is first step and que says we can start from 0 ro 1 
+        so I think it's cost will be same as 1st index cost 
+    4. start to check from 2nd index
+        - we need minimun cost right? 
+        - aplyala [i-1] and [i-2] madhla min cost ghyava lagel
+        - and tyat mala curr index chi cost add karavi lagel
+        - yeah I'm shock too maz dok asa kasa challa te but it's the right approach
+ 
+ 
+ * Pseudo Code :
+ 
+    function minCostClimbingStairs (cost) {
+    
+        -> declare dp
+    
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for(i = 2 to cost.length)
+
+            - get minimum cost between it's prev step and prev of prev step
+            - why? karan bagh mala haviye minimum cost and mala curr cost pn pay karaychiye
+                tyamul mala 2 steps prev chya cost madhe add karavi lagel curr cost and mala tyatla min lagel mhnun me adhich min kadhun ghete
+
+            minCostForPrevSteps = min(dp[i-1], dp[i-2])
+
+            - add one of the prev step cost in currCost
+            dp[i] = cost[i] + minCostForPrevSteps
+        
+        return dp[n]
+    }
+
+
+ */
