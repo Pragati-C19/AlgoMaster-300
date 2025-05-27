fec1adbbe61512621cd2f3bd3c37b860a3d00d76
Pragati Chothe <pragatichothe@gmail.com>
2025-05-27 22:46:03 +0530

    🐛 Debugging 4 | Updated driver function but now I need to think how to adjust the range in helper function?

--- 40. DP/1. 1-DP/Rob2.java
+++ 40. DP/1. 1-DP/Rob2.java
@@ -6,29 +6,18 @@ public class Rob2 {
         
         // Declare variables
         int n = nums.length;
-        int[] dp = new int[n + 1];
+        
+        // if we rob house 0 to n - 2
+        int robHouseZero = dp(nums, 0, n - 2);
 
-        // Assign initial values
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        // if we rob house 1 to n - 1
+        int robHouseOne = dp(nums, 1, n - 1);
 
-        // let's check other houses
-        for (int i = 2; i < n; i++) {
-            
-            int moneyRobToday = nums[i] + dp[i - 2];
-            System.out.println("    - How much money we get by robbing " + i + " today : " + moneyRobToday);
-            
-            dp[i] = Math.max(dp[i - 1], moneyRobToday);
-            System.out.println("    - After cheking " + i + " step DP array looks like : " + Arrays.toString(dp));
+        // Take max between them
+        int maxBetweenTwoRanges = Math.max(robHouseZero, robHouseOne);
 
-        }
-
-        if (dp[0] > dp[1]) {
-
-            return dp[n-2];
-        }
-
-        return dp[n-1];
+        return maxBetweenTwoRanges;
+        
     }
 
     // Helper Function : to check maximum robbed money on days 
