dcc856cd8cd77f5ba1cb33181c5c1bd751378604
Pragati Chothe <pragatichothe@gmail.com>
2025-05-27 22:28:08 +0530

    ⚡️Step 3: Code Writing | My intuition is wrong for 3rd test case.. it dosen't have 3 in it's dp value

--- 40. DP/1. 1-DP/Rob2.java
+++ 40. DP/1. 1-DP/Rob2.java
@@ -4,6 +4,25 @@ public class Rob2 {
     
     public int rob(int[] nums) {
         
+        // Declare variables
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+
+        // Assign initial values
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        // let's check other houses
+        for (int i = 2; i < n; i++) {
+            
+            int moneyRobToday = nums[i] + dp[i - 2];
+            System.out.println("    - How much money we get by robbing " + i + " today : " + moneyRobToday);
+            
+            dp[i] = Math.max(dp[i - 1], moneyRobToday);
+            System.out.println("    - After cheking " + i + " step DP array looks like : " + Arrays.toString(dp));
+                        
+        }
+
         return 0;
     }
 
