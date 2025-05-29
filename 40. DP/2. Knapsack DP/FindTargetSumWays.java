09d13dfe883dc5c1163e1fa51b77719dc21cf1a4
Pragati Chothe <pragatichothe@gmail.com>
2025-05-29 18:57:30 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | let's add this much of data so far

---
+++ 40. DP/2. Knapsack DP/FindTargetSumWays.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class FindTargetSumWays {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have given an integer array nums and a target
+    2. we want to build an expression out of nums
+        - by adding one of the symbols '+' or '-' before each integer in nums
+        - and then concatenate all integers
+    3. example :
+        nums = [2, 1]
+        we can add '+' before 2 and '-' before 1 
+        and concatenate them to build the expression '+2-1'
+    4. return the number of diff expressions that can be build 
+        where after solving that expression will get target
+
+ 
+ * Pattern :
+ 
+    1. Get total sum first 
+        add all num in nums array and add it 
+    2.  
+    2. will set a subStringSum 
+            which is 
+ 
+ 
+ * Pseudo Code :
+ 
+
+
+
+
+ */
