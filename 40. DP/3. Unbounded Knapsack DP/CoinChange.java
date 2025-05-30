cc6ba21bc3d65e08574e13bafedfae7e102b8d1c
Pragati Chothe <pragatichothe@gmail.com>
2025-05-30 15:04:45 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote intuitions.. with one by one build up

---
+++ 40. DP/3. Unbounded Knapsack DP/CoinChange.java
@@ -0,0 +1,131 @@
+public class CoinChange {
+    
+}
+
+/*
+ * Intuitions :
+    
+    1. we have given integer array coins representing coins of diff denominations 
+    2. we have amount array represents total amount of money 
+    3. retrun the fewest number of coins that u need to make up that amount
+        - example we have coins [1, 2, 5] and our amount is 11
+        - then we need extra 3 coins right? to get 11 so will return 3
+        - ata tu mhnshil what abt combination? 3 numbercha coin tr nahiye aplyakde mg?
+        - in que they said we have infinite coins of number given in coins array
+        - so will use 2 5's coins and 1 1's coin -> 1 + 5 + 5 = 11
+        - will not use 2 this time
+    4. if amount of money cannot be made by combinations of coins 
+        - return -1
+        - example we have coins [2] and we need amount as 3
+        - but here we can't combine coins to make 3 if we say 2 + 2 it will be 4 not 3
+        - I know tu mhnshil 2 + 1 karaych simple.. but naa aplyakde ek pn coin 1 nahiye onoy 2 ahet
+ 
+
+ 
+ * Pattern :
+ 
+    1. let's declare a dp of length amount + 1
+        - why? karan mala titki amount haviye coins gheun
+        - me totalSum kadhaycha vichar kelela.. but ti sum kami asel amount pekshya ani apan kadhich amount true nahi karu shaknar
+
+    2. dp[0] = 0
+        - why? karan amount 0 asnyasathi aplyala zero coins ch lagtil for each test case
+
+    3. check for other smounts now and tell if that amount is possible by this nums or not
+        for(amt = 1 to amount)
+            for(coin : coins) 
+
+    4. check if coin <= amt ? 
+        - why? apan check kartoy jr curramt pekshya coin motha asel tr we can't achive to that curramt by that coin
+            tyamul to motha coin skip kartey
+        - if yes then add 1 + dp[amt - coin]
+            why? let's take exmaple
+                - We're at amount = 7    Coin = 3
+                - You already know the minimum coins to form amount = 4 is dp[4] = 2
+                - If we add this coin (3) to the way of forming amount = 4,
+                    you'll reach 7 using 1 (this coin) + dp[4] = 3 coins total.
+ 
+    5. let's visiualize it more
+        - take example ->  coins: [1, 2, 5]    Amount: 7
+        
+        - let's build dp[amount+1]
+        - dp[0] = 0
+        
+        - so our dp looks like
+            index:  0  1  2  3  4  5  6  7
+            value: [0, ∞, ∞, ∞, ∞, ∞, ∞, ∞]
+
+        - let's loop through amount
+            
+            1. amt = 1
+                coin = 1 -> 1 + dp[1-1] = 1
+                coin 2 and 5 are big
+                
+                dp[1] = 1
+
+            2. amt = 2
+                coin = 1 -> 1 + dp[2-1] = 2
+                coin = 2 -> 1 + dp[2-2] = 1
+                coin 5 is big
+                - now see we can use two different coins now how should I know which one to take?
+                    coin 1 = 2  or coin 2 = 1?
+                    it's simple in que they said fewest number of coins 
+                - tyamul apan min gheu 
+                    which is coin 2 = 1 now
+                
+                dp[2] = 1
+
+            3. amt = 3
+                coin = 1 -> 1 + dp[3-1] = 2
+                coin = 2 -> 1 + dp[3-2] = 2
+                coin 5 is big
+                use same logic as above
+                min between both coins are 2 only
+
+                dp[3] = 2
+
+            4. amt = 4
+                coin = 1 -> 1 + dp[4-1] = 3
+                coin = 2 -> 1 + dp[4-2] = 2
+                coin 5 is big
+                use same logic as above
+                min between both coins are 2
+
+                dp[4] = 2
+
+            5. amt = 5
+                coin = 1 -> 1 + dp[5-1] = 3
+                coin = 2 -> 1 + dp[5-2] = 3
+                coin = 5 -> 1 + dp[5-5] = 1
+                use same logic as above
+                min between all coins are 1
+
+                dp[5] = 1
+
+            6. amt = 6
+                coin = 1 -> 1 + dp[6-1] = 2
+                coin = 2 -> 1 + dp[6-2] = 3
+                coin = 5 -> 1 + dp[6-5] = 2
+                use same logic as above
+                min between all coins are 2
+
+                dp[6] = 2
+
+            7. amt = 7
+                coin = 1 -> 1 + dp[7-1] = 3
+                coin = 2 -> 1 + dp[7-2] = 2
+                coin = 5 -> 1 + dp[7-5] = 2
+                use same logic as above
+                min between all coins are 2
+
+                dp[7] = 2
+
+        - return amount = 7 so dp[7] = 2
+
+
+ * Pseudo Code :
+ 
+
+
+
+ */
