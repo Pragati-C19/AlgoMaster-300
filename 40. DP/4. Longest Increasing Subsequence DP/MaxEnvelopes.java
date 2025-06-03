9469ba572e0821c5e482a0b70eb6bd99d5874106
Pragati Chothe <pragatichothe@gmail.com>
2025-06-03 17:58:47 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I guess this will work

--- 40. DP/4. Longest Increasing Subsequence DP/MaxEnvelopes.java
+++ 40. DP/4. Longest Increasing Subsequence DP/MaxEnvelopes.java
@@ -1,5 +1,5 @@
 
-\]vygtfrdeswimport java.util.*;
+import java.util.*;
 
 public class MaxEnvelopes {
     
@@ -22,6 +22,9 @@ public class MaxEnvelopes {
         index       :     0       1       2       3
         envelopes   :   [5,4],  [6,4],  [6,7],  [2,3]
         
+        - let's sort envelope array by weight and height
+            index       :     0       1       2       3
+            envelopes   :   [2,3],  [5,4],  [6,4],  [6,7]
 
         - let's start a dp and set value for dp[0]
             will store number of envolopes we can fit till the currIndex
@@ -34,31 +37,49 @@ public class MaxEnvelopes {
         - set dp[0] = 1     
             at start ya envolope chi length 1 asel  
 
-        - currEnvolope 1 -> weight = 6, height = 4
-            will check all index and then will see if we can fit them in curr
+        - currEnvolope 1 -> weight = 5, height = 4
+            will check all preIndex and then will see if we can fit them in curr
 
-            index = 0 -> currW(6) > indexW(5), currH(4) = indexH(4)   -> we can't take this as currH = indexH
-            index = 2 -> currW(6) = indexW(6), currH(4) < indexH(7)   -> we can't take this as currW = indexW
-            index = 3 -> currW(6) > indexW(2), currH(4) > indexH(3)   -> both weight and size is small of this index so dp[currEnvolope]++
-
-            dp = [1, 2, 1, 1]
-
-        - currEnvolope 2 -> weight = 6, height = 7
-            will check all index and then will see if we can fit them in curr
-
-            index = 0 -> currW(6) > indexW(5), currH(7) > indexH(4)   -> dp[currEnvolope] = 1 + 2 = 2
-            index = 1 -> currW(6) = indexW(6), currH(7) > indexH(4)   -> we can't take this as currW = indexW
-            index = 3 -> currW(6) > indexW(2), currH(7) > indexH(3)   -> both weight and size is small of this index so dp[currEnvolope]++
-
+            preIndex = 0 -> currW(5) > preIndexW(2), currH(4) > preIndexH(3)   -> dp[currIndex] = 1 + dp[0] = 2
+            
             dp = [1, 2, 1, 1]
 
 
+        - currEnvolope 2 -> weight = 6, height = 4
+            will check all preIndex and then will see if we can fit them in curr
+
+            preIndex = 0 -> currW(6) > preIndexW(2), currH(4) > preIndexH(3)   -> dp[currIndex] = 1 + dp[0] = 2
+            preIndex = 1 -> currW(6) > preIndexW(5), currH(4) = preIndexH(4)   -> we can't take this as currH = preIndexH
+            
+            dp = [1, 2, 2, 1]
+
+
+        - currEnvolope 3 -> weight = 6, height = 7
+            will check all preIndex and then will see if we can fit them in curr
+
+            preIndex = 0 -> currW(6) > preIndexW(2), currH(7) > preIndexH(3)   -> dp[currIndex] = 1 + dp[0] = 2
+            preIndex = 1 -> currW(6) > preIndexW(5), currH(7) > preIndexH(4)   -> dp[currIndex] = 1 + dp[1] = 3
+            preIndex = 2 -> currW(6) = preIndexW(6), currH(7) > preIndexH(4)   -> we can't take this as currW = preIndexW
+            
+            dp = [1, 2, 2, 3]
+
+        - dp[n] = 3
 
  
  * Pseudo Code :
  
- 
+    1. declare variables :
+        - n     -> length of envelope array
+        - maxLength     -> It will tell how many envelopes are added in curr evelope
+        - dp            -> it will store length of Russian doll
 
-
+    2. Sort envelope array 
+    3. fill dp initially with 1 
+    4. start nested for lopps
+        for(currIndex = 1 to n)
+            for(prevIndex = 0 to currIndex)
+    5. will check if we can take currIndex's w and h are max than prevIndex's 
+        if yes then will updated the dp[currIndex]
+        
  
- */-+
+ */
