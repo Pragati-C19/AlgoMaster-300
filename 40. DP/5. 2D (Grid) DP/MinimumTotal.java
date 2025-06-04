df2c2295ff3cdf72ab50e2acde7b540aa8af9d31
Pragati Chothe <pragatichothe@gmail.com>
2025-06-04 20:01:02 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | After lot of thinking Wrote intuition on my own without any help.. and I'm happy I think it will work perfectly

---
+++ 40. DP/5. 2D (Grid) DP/MinimumTotal.java
@@ -0,0 +1,118 @@
+import java.util.*;
+
+public class MinimumTotal {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have given a triangle array
+    2. we need to return path sum from top to bottom
+    3. for each step we may move to adj number
+        as in if u are on index i on curr row
+        then will move to either i or index i + 1 on the next row
+ 
+ 
+ * Pattern :
+ 
+    ^ Trace Example :
+
+                    triangle                                         dp                                         dp (optimal logic)                                                                                                                                   
+
+                        2                                             2                                                 2                                                                     
+                    3       4                                2+3=5  /   \  2+4=6                               2+3=5  /   \  2+4=6                                                                                                                                  
+                6       5       7                                  5     6                                           5     5                                                                  
+            4       1       8       3                             /       \                                         /       \                                                                     
+                                                         [5+6=11, [5+5=10, [5+7=12,                            5+6=11 5+5=10 5+7=12                                                                     
+                                                          6+6=12]  6+5=11]  6+7=13]                               min(11,10,12)                                                                     
+                    [i,j] value                               /       |        \                                /       |        \                                                                                                                    
+                                                             11       10       12                              10       10       10                                                                   
+                      [0,0]                                 /         |          \                            /         |          \                                                                  
+                  [1,0]   [1,1]                     [11+4=15, [11+1=12, [11+8=19, [11+3=14,             10+4=14  10+1=11  10+8=18  10+3=13                                 
+              [2,0]   [2,1]   [2,2]                  10+4=14,  10+1=11,  10+8=18,  10+3=13,                      min(14,11,18,13)                                                        
+          [3,0]   [3,1]   [3,2]   [3,3]              12+4=16]  12+1=13]  12+8=20]  12+3=15]               /       |          |        \                                       
+                                                        /        |          |        \                  14       11          18        13                                                                                                                                     
+                                                      14         11         18        13                                                                                                                                                   
+
+        - 11 is th ans bcoz it's a min one
+        
+        - First approach
+            yat me check karat hote ki prevRow madhle nums one by one add karayche currRow chya nums madhe 
+            then tyancha madhun min lihaycha dp madhe
+            but ithe pratek veles last 3 prev nums check karan is not possible
+        
+        - Second approach 
+            to avoid too much sum and all
+            apan pratek dp row la min set karu
+            for(i = 1 to m)
+                for(j = 0 to triangle[i].length)
+
+                    -> he prevIndexPath sathi why use i=1? check out [i-j] value above
+                    prevIndexPath = dp[i-1][j]
+                    
+                    -> Check what will be the sum if we add it in currNum 
+                    sumWeGet = grid[i][j] + prevIndexPath
+
+                    dp[i][j] = min(dp[i][j], sumWeGet)
+
+            after tracing this I think I made a mistake
+                karan if 
+                    i = 1 and j = 0 : grid[1][0] = 3  dp[0][0] = 2
+                        then dp[1][0] = min(max, 3+2) = 5
+                    i = 1 and j = 1 : grid[1][1] = 4  dp[0][0] = 2
+                        then dp[1][1] = min(max, 4+2) = 6
+
+                see me kadhich 5 both side set nahi akru shakle
+
+        - Third Approach
+            apan thod change karu code just a little bit 
+            mala fact top(i-1) nahi check karaychay mala left(j-1) pn check karaychay
+            like 
+                i = 1 and j = 0 : grid[1][0] = 3  dp[0][0] = 2 we don't have any left num here
+                    then dp[1][0] = 3+2 = 5
+                i = 1 and j = 1 : grid[1][1] = 4  
+                    sumFromTop  = grid[i][j] + dp[i-1][j] = grid[1][1] + dp[0][0] = 4 + 2 = 6   
+                    sumLeftNum = dp[i][j-1] = dp[1][0] = 5
+
+                    then dp[1][1] = min(6, 5) = 5
+
+
+
+ * Pseudo Code :
+
+    function minimumTotal (triangle) {
+    
+        -> Declare variables
+            m = triangle.length
+            apan n nahi find karan bcoz each row has diff num of col
+            dp = to store min sum we get from a path (top or left)
+            
+        -> Initially set dp[0][0] as grid[0][0]
+            bcoz it's astarting point
+
+        -> let's set every first col of each row or u can say 0th index of each row
+            where i = varies and j = 0
+            for(i = 1 to m)
+
+                - it dosn't have left num so we are not going to check ki prev num cha dp value small ahe ki nahi te
+                    apan directly top + curr jo yeil to store karu
+
+                    dp[i][0] = dp[i-1] + grid[i][0]
+
+        
+        -> Now check other cells 
+            for(i = 1 to m)
+                for(j = 1 to triangle[i].length)
+
+                    sumFromTop = dp[i-1][j] + grid[i][j]
+                    prevIndexDP = dp[i][j-1]
+
+                    dp[i][j] = min(sumFromTop, prevIndexDP)
+        
+        -> At end return dp[m-1][n-1]
+    
+    }
+
+
+ */
