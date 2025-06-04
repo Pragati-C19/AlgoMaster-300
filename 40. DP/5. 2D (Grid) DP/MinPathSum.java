6c9f52c77b40b961b6a1355f876c2f4e75787b92
Pragati Chothe <pragatichothe@gmail.com>
2025-06-04 16:39:28 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Wrote intuition on my own without any help.. and I'm happy I think it will work perfectly

---
+++ 40. DP/5. 2D (Grid) DP/MinPathSum.java
@@ -0,0 +1,136 @@
+import java.util.*;
+
+public class MinPathSum {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have given a grid (mxn) filled with positive numbers
+    2. We need to find path from top-left(0,0) to bottom-right(m-1, n-1)
+    3. but there is a catch.. aplyala tya path madhlya saglya nums chi sum min haviye
+    4. we can only move down or right
+
+ * Pattern :
+
+    1. Declare variables
+        - m, n
+        - dp    -> to store min sum we get from a path (top or left)
+        
+    2. apan nehmi (0,0) pasun start karnar ahe so let's set it's dp first
+        - (0,0) parynt pohochaycha ekach way and till the sum grid[0][0] ch asel 
+        - dp[0][0] = grid[0][0]
+
+    3. let's check example an see what's happening
+
+                      Grid                                        dp                                                                                            
+                                                                               
+                |                                   |        1+3      4+1                                                                                                                                  
+              0 |   1   3   1                     0 |     1 -----> 4 -----> 5                                                                                                                               
+              1 |   1   5   1                       |     |        |        |                                                                                                                         
+              2 |   4   2   1                       | 1+1 |    4+5 |    5+1 |                                                                                                                                          
+            ----|---------------                    |     |        |        |                                                                                                                                                  
+                |   0   1   2                       |     v  2+5   v  7+1   v                                                                                                                                          
+                                                  1 |     2 -----> 7 -----> 6                                           
+                                                    |     |        |        |                                           
+                                                    | 2+4 |    7+2 |    6+1 |                                        
+                                                    |     |        |        |                                    
+                                                    |     v  6+2   v  8+1   v                                    
+                                                  2 |     6 -----> 8 -----> 7                                           
+                                                    |                                                                   
+                                                ----|-------------------------------                                    
+                                                    |     0        1        2        
+                                              
+    
+    -> First Row : i = 0
+        - to reach [0,1] there is only one way from left[0,0]  
+            so update [0,1] cell as dp[0][0] + grid[0][1] = 1 + 3 = 4
+
+        - to reach [0,2] there is only one way from left[0,1] 
+            so update [0,2] cell as dp[0][1] + grid[0][2] = 4 + 1 = 5
+
+    -> Second Row : i = 1
+        - to reach [1,0] there is only one way from top[0,0]     
+            so update [1,0] cell as dp[0][0] + grid[1][0] = 1 + 1 = 2
+            
+        - to reach [1,1] there is Two ways from top[0,1] from left[1,0]
+            so we need to take min between both paths
+            dp[0][1] + grid[1][1] = 4 + 5 = 9
+            dp[1][0] + grid[1][1] = 2 + 5 = 7
+            so update [1,1] cell as 7
+        
+        - to reach [1,2] there is Two ways from top[0,2] from left[1,1]
+            so we need to take min between both paths
+            dp[0][2] + grid[1][2] = 5 + 1 = 6
+            dp[1][1] + grid[1][2] = 7 + 1 = 8
+            so update [1,2] cell as 6
+    
+    -> Third Row : i = 2
+        - to reach [2,0] there is only one way from top[1,0]          
+            so update [2,0] cell as dp[1][0] + grid[2][0] = 2 + 4 = 6
+
+        - to reach [2,1] there is Two ways from top[1,1] from left[2,0]
+            so we need to take min between both paths
+            dp[1][1] + grid[2][1] = 7 + 2 = 9
+            dp[2][0] + grid[2][1] = 6 + 2 = 8
+            so update [2,1] cell as 8
+
+        - to reach [2,2] there is Two ways from top[1,2] from left[2,1]
+            so we need to take min between both paths
+            dp[1][2] + grid[2][2] = 6 + 1 = 7
+            dp[2][1] + grid[2][2] = 8 + 1 = 9
+            so update [2,2] cell as 7  
+
+    -> so at the end will return the um whch is 7
+
+
+ * Pseudo Code :
+
+    function minPathSum (grid) {
+    
+        -> Declare variables
+            m, n
+            dp
+
+        -> Initially set dp[0][0] = grid[0][0]
+            karan (0,0) is a starting point so tyachi sum grid[0][0] itkich asel
+
+
+        -> Check first row : i = 0 and j = varies
+            me directly nested for loop lavaycha vichar karat hote but I have learned ki i-1 or j-1 la issue yeto first row or col madhe
+            apan j = 1 kartoy karan jevha j-1 karu tevha index bound out cha error nahi yenar
+
+            for(j = 1 to n)
+                - set dp's values by adding prev dp sum in currCell
+                    also first row madhe top (i-1) kadun kahich yet nahi just left(j-1) kadun yet
+
+                dp[0][j] = dp[0][j-1] + grid[0][j]
+
+
+        -> Check first col : i = varies and j = 0
+            me directly nested for loop lavaycha vichar karat hote but I have learned ki i-1 or j-1 la issue yeto first row or col madhe
+            apan i = 1 kartoy karan jevha i-1 karu tevha index bound out cha error nahi yenar
+
+            for(i = 1 to m)
+                - set dp's values by adding prev dp sum in currCell
+                    also first col madhe left (j-1) kadun kahich yet nahi just top(i-1) kadun yet
+
+                dp[i][0] = dp[i-1][0] + grid[i][0]
+
+        
+        -> let's check remaining cells
+            for(i = 1 to m)
+                for(j = 1 to n)
+
+                    sumFromTop = dp[i-1][j] + grid[i][j]
+                    sumFromLeft = dp[i][j-1] + grid[i][j]
+
+                    dp[i][j] = min(sumFromTop, sumFromLeft)
+
+                
+        -> At end return dp[m-1][n-1]
+    
+    }
+
+ */
