9d39be6c532dbba1d3c9497fef856fe2273a09ed
Pragati Chothe <pragatichothe@gmail.com>
2025-06-07 17:09:11 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | Took help from yt to understand the pattern let's try to solve the que

---
+++ 40. DP/5. 2D (Grid) DP/MaxCoins.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class MaxCoins {
+
+}
+
+/*
+ * Intuitions :
+ 
+    1. we have given n ballons (0 to n - 1)
+    2. each balloon has a number on it which tells in nums array
+    3. we need to burst all balloons but there is a catch
+        if we bust the ith balloon 
+            will get nums[i-1] * nums[i] * nums[i+1] coins
+        if i-1 and i+1 goes out of bound 
+            treat it as if number on those balloons are 1
+
+    4. need to return maximum coins we can collect by bursting balloons
+ 
+ 
+ * Pattern :
+ 
+    1. Start with entire Array or Block
+	2. will mark them i and j 
+		where i = start
+		          j = end 
+	3. Try all partitions 
+		- Run A loop to try those partitions
+		- take example : [ A, B, C, D]
+		partitions are in format of ( i ) ( j )
+			(A) (B,C,D)
+			(A,B) (C,D)
+			(A,B,C) (D)
+	4. Return the best possible two partitions
+		- Two partitions means ( i ) ( j )
+
+
+    -> It seems same as binary search 
+        like find start and end 
+        then check mid and do operations on both sides of mid
+        while loop till i <= j
+	
+	-> Just ithe dp create karun tyat value takaychiye
+
+    ^ Check out below link :
+        https://youtu.be/Yz4LlDSlkns?si=FlpGioHQz7mhsI_1
+
+ 
+ 
+ * Pseudo Code :
+ 
+
+
+ */
