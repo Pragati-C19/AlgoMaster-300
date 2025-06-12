fc79989a53eb4010bf92706e1b1585fe611cdd92
Pragati Chothe <pragatichothe@gmail.com>
2025-06-12 22:55:21 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | tring to understand the problem will check it tommorrow

---
+++ 40. DP/6. String DP/WordBreak.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class WordBreak {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. we have given a string(s) and a dictornary of strings(wordDict)
+    2. return true if s can be segmented into a space-seperated sequence of one or more dictionary words
+        what does it means?
+            example : s = "leetcode", wordDict = ["leet","code"]
+                Return true because "leetcode" can be segmented as "leet code".
+    3. we can use same word in dictionary multiple times
+ 
+ * Pattern :
+    
+    1. 
+ 
+ 
+ 
+ 
+ * Pseudo Code :
+ 
+ 
+ 
+ 
+ */
