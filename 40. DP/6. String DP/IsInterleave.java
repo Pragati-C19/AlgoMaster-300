3e11dff71a55a295bbd64467030b1fbc5c868ff7
Pragati Chothe <pragatichothe@gmail.com>
2025-06-13 23:16:05 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I took help to understand problem and solve it

---
+++ 40. DP/6. String DP/IsInterleave.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class IsInterleave {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have given 3 strings s1, s2, s3
+    2. we need to find whether s3 is formed by an interleaving of s1 and s2
+    3. interleaving of string means s and t is a configuration 
+        where s and t are divided into n and m substrings respectively
+        example : s = s1 + s2 + s3 + ... + sn
+                  t = t1 + t2 + t3 + ... + tm
+                  |n - m| <= 1
+                  so the interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+    4. a + b is the cncatenation of strings a and b
+    5. I don't understand what is this que want exactly
+ 
+ 
+ * Pattern :
+ 
+    1. mala itka tr samjtay ki s3 madhun aplyala one by one check karaych ahe
+        apan ji substring gheu ti s3 madhli asel like
+            s3.substring(j, i)
+
+    2. then apan tya substring la check karu s1 madhe ahe ka or s2 madhe ahe ka te
+    3. took help from gpt and wrote this code
+    
+ 
+ * Pseudo Code :
+ 
+    function isInterleave(s1, s2, s3) {
+    
+        -> Declare variables
+            m, n                -> length of s1 and s2 respectively
+            boolean[][] dp      -> it will store all possible ways to form s3[0.. i+j-1] using s1[0..i-1] and s2[0..j-1]
+    
+
+        -> aplyala s1 and s2 che sagle chars use karaychet so
+            s1.length + s2.length == s3.length asel nehmi 
+            jr nasel te tr adhich return false karun de
+
+
+        -> Define initial value in dp
+            empty strings s1 and s2 can form empty s3
+            dp[0][0] = true
+
+
+        -> Will initialize first row 
+            only char from s2 are used
+            for(j = 1 to n)
+                dp[0][j] = dp[0][j-1] && s2.charAt(j-1) == s3.charAt(j-1)
+
+
+        -> Will initialize first col 
+            only char from s1 are used
+            for(i = 1 to m)
+                dp[i][0] = dp[i-1][0] && s1.charAt(i-1) == s3.charAt(i-1)
+
+
+        -> Check Remaining chars
+            for(i = 1 to m)
+                for(j = 1 to n)
+
+                    - will take either char from s1 or char from s2
+                        charFromS1 = dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1)
+                        charFromS2 = dp[i][j-1] && s2.charAt(i-1) == s3.charAt(i+j-1)
+
+                    - dp[i][j] = charFromS1 || charFromS2
+
+
+        -> Final answer: can s3 be built from all of s1 and s2?
+            dp[m][n]
+
+    }
+
+
+ */
