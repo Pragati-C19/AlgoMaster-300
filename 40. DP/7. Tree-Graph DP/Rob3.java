ed6942677f452b3714db8b514a65bf3ff92c679c
Pragati Chothe <pragatichothe@gmail.com>
2025-06-17 19:10:39 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run DP on VsCode, this won't be needed to add in leetcode

--- 40. DP/7. Tree-Graph DP/Rob3.java
+++ 40. DP/7. Tree-Graph DP/Rob3.java
@@ -2,6 +2,91 @@ import java.util.*;
 
 public class Rob3 {
     
+   // Definition for a binary tree node.
+   public static class TreeNode {
+      
+      int val;
+      TreeNode left;
+      TreeNode right;
+      
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+
+   }
+
+   // Driver Function
+   public int rob(TreeNode root) {
+        
+      return 0;
+   }
+
+   // Helper Function : to get values of nodes should be rob and non rob
+   private int[] dfs(TreeNode currNode) {
+       
+      return new int[]{0,0};
+   }
+
+
+   // Helper Function : to build a tree from an array (for testing)
+   public static TreeNode buildTree(Integer[] nodes) {
+        
+      // Node is empty
+      if (nodes.length == 0 || nodes[0] == null) return null;
+      
+      TreeNode root = new TreeNode(nodes[0]);
+      Queue<TreeNode> queue = new LinkedList<>();
+      
+      queue.add(root);
+      
+      int i = 1;  // Start from second element
+
+      while (!queue.isEmpty() && i < nodes.length) {
+            
+         TreeNode parent = queue.poll();
+            
+         // Assign left child
+         if (nodes[i] != null) {
+            
+            parent.left = new TreeNode(nodes[i]);
+            queue.add(parent.left);
+         }
+         i++;
+         
+         // Assign right child (check if there's still an element)
+         if (i < nodes.length && nodes[i] != null) {
+            
+            parent.right = new TreeNode(nodes[i]);
+            queue.add(parent.right);
+         }
+         i++;
+      }
+
+      return root;
+   }
+
+   public static void main(String[] args){
+      
+      Rob3 solution = new Rob3();
+
+      // First Example
+      Integer[] treeArray1 = {3,2,3,null,3,null,1};
+      TreeNode root1 = buildTree(treeArray1);
+      System.out.println("Result1 -> " + solution.rob(root1) + "\n");
+
+      // Second Example
+      Integer[] treeArray2 = {3,4,5,1,3,null,1};
+      TreeNode root2 = buildTree(treeArray2);
+      System.out.println("Result2 -> " + solution.rob(root2) + "\n");
+      
+   }
+
 }
 
 /*
@@ -166,7 +251,7 @@ public class Rob3 {
          notRob = max(left[notRob], left[rob]) + max(right[notRob], right[rob])
 
       -> return rob and notRob
-      
+
    }
 
 
