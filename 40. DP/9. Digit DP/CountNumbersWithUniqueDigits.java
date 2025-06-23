dd750e295c5702621790b1dc7b4a2d2304ed0dc0
Pragati Chothe <pragatichothe@gmail.com>
2025-06-23 23:11:39 +0530

    🐛 Debugging 5 | didn't use dp and code still works for given est cases ig after submit it will pass all test cases

--- 40. DP/9. Digit DP/CountNumbersWithUniqueDigits.java
+++ 40. DP/9. Digit DP/CountNumbersWithUniqueDigits.java
@@ -10,48 +10,31 @@ public class CountNumbersWithUniqueDigits {
             return 1;
         }
 
-        // Declare variables
-        int[] dp = new int[n + 1];
-
-        // Base Case :
-        dp[0] = 1;
-        dp[1] = 10;
-
-
         // Add initial value to total unique numbers
-        // will take only dp[1] bcoz 0 to 9 are coverd we don't need write dp[0]
-        int totalUniqueNumbers = dp[1];
+        // will take numbers as 0 to 9
+        int totalUniqueNumbers = 10;
 
-        // calculate total unique numbers of digitLength > 1
-        for (int digitLength = 2; digitLength <= n; digitLength++) {
+        // first digit has 9 options :  [1-9] cannot be 0
+        int firstDigit = 9;
+
+        // for remaining (digitLength - 1) digits, we choose from remaining unique digits
+        // remaining choices for second digit are except first but we have 0 
+        int remainingChoices = 9;
+
+        
+        // for next digit if we have more than 2 digits like greater than 100, 1000 like that
+        for (int i = 2; i <= n; i++) {
             
-            // first digit has 9 options :  [1-9] cannot be 0
-            int firstDigit = 9;
+            // Multiply with the number of digits left to pick from
+            firstDigit *= remainingChoices;
 
-            // for remaining (digitLength - 1) digits, we choose from remaining unique digits
-            // remaining choices for second digit are except first but we have 0 
-            int remainingChoices = 9;
-
-            // for next digit if we have more than 2 digits like greater than 100, 1000 like that
-            for (int i = 1; i < digitLength; i++) {
-                
-                // System.out.println("    Digit Position " + (i + 1) + ": \n      firstDigit = " + firstDigit + " * " + remainingChoices);
-
-                // Multiply with the number of digits left to pick from
-                firstDigit *= remainingChoices;
-
-                // Reduce remaining digits for next position
-                remainingChoices--;
-            }
-
-            // Save tha count in dp
-            dp[digitLength] = firstDigit;
-
+            // Reduce remaining digits for next position
+            remainingChoices--;
+                        
             // add that count in total
             totalUniqueNumbers += firstDigit;
-
-
         }
+
 
         return totalUniqueNumbers;
     }
