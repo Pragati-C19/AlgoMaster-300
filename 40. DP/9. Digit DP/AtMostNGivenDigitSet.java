ac54a6e1ca15e5d09e24b674f5c816a3408e65b1
Pragati Chothe <pragatichothe@gmail.com>
2025-06-25 00:12:12 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I think it will be right approach

--- 40. DP/9. Digit DP/AtMostNGivenDigitSet.java
+++ 40. DP/9. Digit DP/AtMostNGivenDigitSet.java
@@ -49,9 +49,91 @@ public class AtMostNGivenDigitSet {
             or me jr 234 check karat asel tyat mala husdred place lagtey but mala sagle nums nakoy with hundreds place
         - tyamul mala n.length al ki pratek veles check karav lagel ki number less than ahe ki nahi 
         - ata kr vichar kaskay karnar ahe without using for(i = 0 to n)
-    10. 
+    
 
- 
+    ^ Start Fresh :
+
+    1. Data I understood so far :
+        
+        -> Phase 1 : Count numbers with less digits than n
+            - if n = 100 (3 digits) then :
+                count all 1-digit numbers -> m
+                count all 2-digit numbers -> m^2 or m * m
+                total = m + (m*m)
+            - Use Power here it will be easier instead of m * m
+                Math.pow(m, lengthOfN)
+
+        -> Phase 2 : Count numbers with the same number of digits as n
+            - Here I'm stuck 
+            - let's take example n = 100
+                nLength = 3
+                ata aplyala number build karaychay ha with given digits but 100 pekshya extend nahi zal pahije
+            
+            - prate position la kay karaych ata ?
+                1. let's say aplyala xyz number build karaychay where x,m y, z are choosen from digit array
+                2. At each position (from left to right), we check:
+                    - How many digits in digits[] are less than the current digit in n
+                    - That tells us how many options we can use to create smaller numbers at this place
+                3. If the current digit in n exists in our digit array, we can proceed to the next digit
+                4. If not, we must stop there (since we can't match n anymore)
+
+            - Dry Run :
+                digits = ["1", "2", "5", "7"], n = 234
+
+                - total till nLength = 2 is 
+                    TotalCount = 4 + (4 * 4) = 20
+                - For nLength = 3 let's check one by one digit and get smallest from digits
+                     will check from left to right  2, 3, 4
+
+                - check index = 0, digitOfN = 2
+                    loop over digits
+                        
+                    - d = 1 : 
+                        d < digitOfN ?  ->  1 < 2  -> Yes
+                        means we can choose any 2-digit number for remianing Places 
+
+                        currCount = m * m = 4 * 4 = 16
+                        TotalCount = TotalCount + currCount = 20 + 16 = 36
+
+                    - d = 2 : 
+                        d == digitOfN  -> 2 == 2  -> Now check for next postion 
+                        
+                        check index = 1, digitOfN = 3
+                            - d = 1
+                                d < digitOfN ?  ->  1 < 3  -> Yes
+                                means we can choose any 1-digit number for remianing Place
+
+                                currCount = m = 4
+                                TotalCount = TotalCount + currCount = 36 + 4 = 40
+
+                            - d = 2
+                                d < digitOfN ?  ->  2 < 3  -> Yes
+                                means we can choose any 1-digit number for remianing Place
+
+                                currCount = m = 4
+                                TotalCount = TotalCount + currCount = 40 + 4 = 44
+
+                            - d = 5 :
+                                d < digitOfN ?  ->  5 > 3  -> No 
+                                break will not count this 
+
+                            - d = 7 
+                                d < digitOfN ?  ->  7 > 3  -> No 
+                                break will not count this 
+
+                    - d = 5 :
+                        d < digitOfN ?  ->  5 > 2  -> No 
+                        break will not count this 
+
+                    - d = 7 
+                        d < digitOfN ?  ->  7 > 2  -> No 
+                        break will not count this 
+
+                - We didin't check index = 3 bcoz we didn't reach there 
+                    jr mala digits array madhe 3 asta and second position la 3 == 3 zala asta
+                    tr unitPlace (index = 3) pn check karavi lagli asti
+                    
+                    
  
  * Pseudo Code :
  
@@ -78,6 +160,59 @@ public class AtMostNGivenDigitSet {
 
             -> return totalCountOfNumbers
         
+        }
+
+
+    2nd Approach :
+
+        function atMostNGivenDIgitSet(digits, n) {
+        
+            -> Declare variables
+               nString      - convert int n to string bcoz digits are String[]
+               nLength      - length of n to know how many digits or positions it has
+               m            - length of digits Array
+               totalCount   - initially it is 0
+
+
+            -> Phase 1 : Count numbers with less digits than n
+                for(i = 0 to nLength)
+                    
+                    - All d-digit numbers using m digits
+                        totalCount += m^i 
+
+            -> Call recursion
+                totalCount += dfs(0)
+
+            -> return totalCount
+
+        }
+
+
+
+        function dfs(positionIndex) {
+        
+            -> Base Case : if positionIndex == nLength
+                if (positionIndex == nLength)
+                    return 1        // matched all digits -> valid number
+
+            -> declare currCount = 0
+
+            -> check each digits of digits array
+                for (d : digits)
+
+                    - check if d < digitOfN ? if Yes updated count, if not then check next condition 
+                        if (d < nString[positionIndex])
+                            count += m^(nLength - positionIndex - 1)
+
+                    - check if d == digitOfN ? if Yes then call recursion
+                        else if(d == nString[positionIndex])
+                            count += dfs(positionIndex + 1)
+
+                    - if above both conditions are not valid then break for loop
+
+
+            -> return count
+                        
         }
 
 
