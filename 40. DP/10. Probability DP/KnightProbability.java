b4ea1b3d68c01668bbfb900fab4fa1dc95861356
Pragati Chothe <pragatichothe@gmail.com>
2025-06-28 19:48:20 +0530

    📝 Step 1 : Intuition, Pattern or Pseudo code | I wrote logic on my own.. and I think it will work no matter what

---
+++ 40. DP/10. Probability DP/KnightProbability.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class KnightProbability {
+    
+}
+
+/*
+ * Intuitions :
+ 
+    1. We have a chessboard of n x n, a knight starts at cell (row, col)
+    2. Knight can make exactly k moves
+    3. chess Knight has 8 posible moves 
+        - he same chess madhe knight jasa chalto tasa ahe
+
+                |               
+              0 |   0   J   0   J   0      
+              1 |   J   0   0   0   J      
+              2 |   0   0   K   0   0               J - Knight can Jump on that cell
+              3 |   J   0   0   0   J               K - Knight is on this cell
+              4 |   0   J   0   J   0     
+            ----|------------------------- 
+                |   0   1   2   3   4
+                |
+                
+        - if Knight is at cell (2, 2), we can go to
+            [1,0], [0,1], [0,3], [1,4]
+            [3,0], [4,1], [4,3], [3,4]
+            
+        - So jr me i, j chya values madhe ghetal tr 
+            if Knight is at (i, j), we can go to below 8 directions
+            
+            [i-1, j-2]  [i-2, j-1]
+            [i-1, j+2]  [i-2, j+1]
+            [i+1, j-2]  [i+2, j-1]
+            [i+1, j+2]  [i+2, j+1]
+
+    4. Knight can choose one of eight possible moves even if it ges out of chessBoard
+    5. Knight will keep moving until it has made exactly k moves 
+        or it moves out of chessBoard
+    6. return probability that knight remains on board after it has stopped moving
+
+ 
+ * Pattern :
+ 
+    1. Declare a directionMatrix Array with those 8 directions
+    2. Conditions when we need to stop 
+        - if knight goes out of board
+            kevha jail to?.. when i and j are 
+            i < 0 or i >= n or j < 0 or j >= n
+            
+            will return 0
+
+        - jr above condition nahi hotye tr apan check karu k = 0 zalay ka te?
+            karan zala asel tr means our knight is on board so far
+            me asa ka mhntey ? bcoz apan adhich check kartoy ki knight ahe board vr ki nahi te
+
+            k == 0  -> return 1
+
+    3. then apan same cell for same moves nako parat parat check karayla 
+        like me atta [1,1] check keli for 3 moves left when my knight is at [2,3]
+        mg me kay parat Probability kadhat basu tyachyasathi?
+
+        tyamul ithe DP use hoil jo probability store karel for (i, j, k)
+        and if those are visited apan direct store keleli probability return karu
+
+    4. Check all directions from directionMatrix for each cell
+        and will add those probability in our currProbability 
+        
+    5. I think it's simple logic now.. will use dfs to check all directions
+
+ 
+ * Pseudo Code :
+ 
+    function knightProbability (n, k, row, column) {
+    
+        -> Declare Variables
+            dp[i][j][k]     - to store probabilty for[i,j] cell in k moves
+            visited         - to check if (i, j, k) is visited or not
+            directionMatrix - it stores all 8 directions we can move to
+
+        -> Call DFS function 
+            and as per the que will start from given row, column
+            dfs(row, column, k, n) 
+    
+        -> return probability
+    
+    }
+
+    function dfs (i, j, k, n) {
+    
+        -> Base Case : if cell is out of chess board means knight is out of board too
+            if(i < 0 or i >= n or j < 0 or j >= n)
+                return 0
+
+        -> if cells are not out of chess board and k == 0 we can return probability as 1 
+            bcoz we are successefully completed the event
+            if(k == 0)
+                return 1
+
+        -> if we have already visited this cell with k moves?
+            if(visited[i][j][k])
+                return dp[i][j][k]
+
+        -> Check all 8 directions
+            for(dir : directionMatrix)
+
+                x = i + dir
+                y = j + dir
+
+                currProbability += dfs(x, y, k-1, n)
+
+        -> Add that currProbability in dp
+        
+        -> return currProbability
+    
+    }
+
+
+ */
