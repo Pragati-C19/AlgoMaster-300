cf67c0a9d1d13cdfdf273f864724bfaf7a6bc715
Pragati Chothe <pragatichothe@gmail.com>
2025-07-05 19:41:24 +0530

    🌱 Step 2 : Basic setup added | Wrote basic code to run que on VsCode, this won't be needed to add in leetcode | it's not just basic setup it has brute force I wrote on leetcode

--- 42. Binary Indexed Tree or Segment Tree/NumArray.java
+++ 42. Binary Indexed Tree or Segment Tree/NumArray.java
@@ -1,7 +1,53 @@
 import java.util.*;
 
 public class NumArray {
-    
+   
+    // Globally Declare variable
+    private int[] numsArray;
+
+    public NumArray(int[] nums) {
+
+        // Assign value here
+        numsArray = nums;
+    }
+
+    public void update(int index, int val) {
+
+        // update value of index 
+        numsArray[index] = val;
+    }
+
+    public int sumRange(int left, int right) {
+
+        // initialize sum as 0
+        int sum = 0;
+
+        // check all nums in range [left, right]
+        for (int i = left; i <= right; i++) {
+
+            // add that num in the sum
+            sum += numsArray[i];
+        }
+        
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        
+        // Initialize the array
+        NumArray solution = new NumArray(new int[]{1, 3, 5});
+        
+        // First query
+        System.out.println(solution.sumRange(0, 2)); // Output: 9
+        
+        // Second query
+        solution.update(1, 2);  // Now the array becomes [1, 2, 5]
+        
+        // Third query
+        System.out.println(solution.sumRange(0, 2)); // Output: 8
+    }
+
 }
 
 /*
@@ -35,7 +81,31 @@ public class NumArray {
  
  * Pseudo Code :
  
-  
+    1. Brute Force :
 
+        - all 15/16 test cases are passed
+        - just last 16th one had TLE
+
+    class NumArray {
+
+        private int[] nums;
+
+        public NumArray(int[] nums) {
+            this.nums = nums;
+        }
+
+        public void update(int index, int val) {
+            nums[index] = val;
+        }
+
+        public int sumRange(int left, int right) {
+            int sum = 0;
+            for (int i = left; i <= right; i++) {
+                sum += nums[i];
+            }
+            return sum;
+        }
+
+    }
 
  */
