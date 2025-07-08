dc232c324007472cb445e758166140ab90aed798
Pragati Chothe <pragatichothe@gmail.com>
2025-07-08 15:20:04 +0530

    🐛 Debugging 3 | after submiting one test case is wrong.. the problem I mention it was bcoz of that

--- 43. Maths or Geometry/TrailingZeroes.java
+++ 43. Maths or Geometry/TrailingZeroes.java
@@ -67,6 +67,8 @@ public class TrailingZeroes {
         System.out.println(" Result4 -> " + solution.trailingZeroes(16) + "\n");     // 0
         System.out.println(" Result5 -> " + solution.trailingZeroes(00000) + "\n");      // 0
         
+        System.out.println(" Result6 -> " + solution.trailingZeroes(13) + "\n");     // 2
+
     }
 
 }
@@ -110,6 +112,10 @@ public class TrailingZeroes {
             [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 1932053504, 1278945280, 2004310016, 2004189184]
 
         - now I'm confused how will I handle n > 16 
+
+        - ohk the problem I mention it's from 13 onwards nums not 16 onwards
+            13! = 6,227,020,800 
+            my ans is 1932053504
         
 
  * Pseudo Code :
