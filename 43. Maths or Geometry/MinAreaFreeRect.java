4e6e107119b623ad8c460eba7848cd08e1316ff4
Pragati Chothe <pragatichothe@gmail.com>
2025-07-10 18:36:53 +0530

    🐛 Debugging 4 | After submission we are getting wrong ans for 4th test case

--- 43. Maths or Geometry/MinAreaFreeRect.java
+++ 43. Maths or Geometry/MinAreaFreeRect.java
@@ -159,6 +159,18 @@ public class MinAreaFreeRect {
         };
         System.out.println(" Result3 -> " + solution.minAreaFreeRect(points3) + "\n");    // 0
 
+        int[][] points4 = {
+            {3,1},
+            {1,1},
+            {0,1},
+            {2,1},
+            {3,3},
+            {3,2},
+            {0,2},
+            {2,3}
+        };
+        System.out.println(" Result4 -> " + solution.minAreaFreeRect(points4) + "\n");    // 2.0
+        
     }
 
 }
